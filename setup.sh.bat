942cce2961175e3a7b2b72b8b4fd524874754556
Dan Cristian <dan.cristian@gmail.com>
2015-03-22 21:52:15 +0200

    script

--- setup.sh.bat
+++ setup.sh.bat
@@ -3,5 +3,3 @@
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python -c "import plotly; plotly.tools.set_credentials_file(username='dancri77', api_key='lw2w6fz9xk',
-    stream_ids=['15ytl1hrhc', '24w1i6s6bs'])"
