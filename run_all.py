c2e1b9ea41434d386518edaaabe6869ac91c17aa
dcristian <dan.cristian@gmail.com>
2015-03-23 00:06:37 +0200

    start on all int

--- run_all.py
+++ run_all.py
@@ -3,6 +3,6 @@ __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 from main import app
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0')
     print 'App EXIT'
 
