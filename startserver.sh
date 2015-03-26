2bf41ed2149708dbc008c55d40c4bb733cc62984
dcristian <dan.cristian@gmail.com>
2015-03-26 22:46:57 +0200

    db

--- startserver.sh
+++ startserver.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 source venv/bin/activate
-echo Enter mem[Enter] to run db in memory. Default is from disk
-read -t 3 answer
-if [ $? == 0 ]; then
-    echo "Selected"
-else
-    echo "Can't wait anymore!"
+#echo Enter mem[Enter] to run db in memory. Default is from disk
+#read -t 3 answer
+#if [ $? == 0 ]; then
+#    echo "Selected"
+#else
+#    echo "Can't wait anymore!"
     answer=disk
-fi
-echo "Your answer is: $answer"
+#fi
+#echo "Your answer is: $answer"
 python run_all.py $answer
