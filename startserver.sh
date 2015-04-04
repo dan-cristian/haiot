42967f7d5b51756a36c5a1e59d285870abfd8dfc
dcristian <dan.cristian@gmail.com>
2015-04-05 00:02:31 +0300

    auto module update

--- startserver.sh
+++ startserver.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 #echo Enter mem[Enter] to run db in memory. Default is from disk
 #read -t 3 answer
 #if [ $? == 0 ]; then
@@ -9,7 +8,27 @@
 #answer=disk
 #fi
 #echo "Your answer is: $answer"
-scripts/stopserver.sh
-source venv/bin/activate
-python run_all.py disk
-echo "program exit with code $?"
+
+function run_app {
+    scripts/stopserver.sh
+    source venv/bin/activate
+    python run_all.py disk
+    exit_code=$?
+    echo "Program exit with code $exit_code"
+}
+
+must_run=true
+while $must_run; do
+    run_app
+    if [ $exit_code == 131 ]; then
+        echo "Restarting app"
+    fi
+    if [ $exit_code == 132 ]; then
+        echo "Upgrading app"
+        git pull --no-edit
+    fi
+    if [ $exit_code == 133 ]; then
+        echo "Shutdown app"
+        must_run=false
+    fi
+done
