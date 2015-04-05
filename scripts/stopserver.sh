e1869fe5456e8659207c40ab1605881f17c56274
dcristian <dan.cristian@gmail.com>
2015-04-05 21:18:24 +0300

    stop script

--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 proc_pid=$(ps w | grep '[p]ython run_all.py' | awk '{print $1}')
-echo Killing proc id [$proc_pid]
-kill $proc_pid
+if [ -n "$proc_pid" ]; then
+    echo Killing proc id [$proc_pid]
+    kill $proc_pid
+else
+    echo Program is not running, nothing to stop
+fi
