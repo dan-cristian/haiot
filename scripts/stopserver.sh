29e3688c1194df794a5359934870bd15d362c48e
dcristian <dan.cristian@gmail.com>
2015-04-10 21:20:05 +0300

    rfx additions

--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 proc_pid=$(ps w | grep '[p]ython run_all.py' | awk '{print $1}')
 if [ -e "$proc_pid" ]; then
+    echo No pid found with ps w, trying now with ps wx
     proc_pid=$(ps wx | grep '[p]ython run_all.py' | awk '{print $1}')
 fi
 if [ -n "$proc_pid" ]; then
