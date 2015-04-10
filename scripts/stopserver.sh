dfcccfc1ec1b65d3ee997ceba258f2944aac908e
dcristian <dan.cristian@gmail.com>
2015-04-10 21:17:25 +0300

    rfx additions

--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 proc_pid=$(ps w | grep '[p]ython run_all.py' | awk '{print $1}')
+if [ -e "$proc_pid" ]; then
+    proc_pid=$(ps wx | grep '[p]ython run_all.py' | awk '{print $1}')
+fi
 if [ -n "$proc_pid" ]; then
     echo Killing proc id [$proc_pid]
     kill $proc_pid
