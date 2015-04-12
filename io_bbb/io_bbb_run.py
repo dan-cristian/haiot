002bf4c896ebee0aba578c3042052406425788c1
dcristian <dan.cristian@gmail.com>
2015-04-12 10:03:07 +0300

    alarm bbb

--- io_bbb/io_bbb_run.py
+++ io_bbb/io_bbb_run.py
@@ -10,13 +10,13 @@ from main.admin import models
 #https://learn.adafruit.com/setting-up-io-python-library-on-beaglebone-black/using-the-bbio-library
 try:
     import Adafruit_BBIO.GPIO as GPIO
+    import_module_exist = True
 except:
     logging.critical('Module Adafruit_BBIO.GPIO is not installed, module will not be initialised')
-
-import_module_psutil_exist = False
+    import_module_exist = False
 
 def register_gpios():
-    global import_module_psutil_exist
+    global import_module_exist
     #global zone_alarm_list
     zone_alarm_list = models.ZoneAlarm.query.all()
     for zonealarm in zone_alarm_list:
@@ -60,8 +60,8 @@ def init():
 
 def thread_run():
     logging.debug('Processing Beaglebone IO')
-    global import_module_psutil_exist
-    if not import_module_psutil_exist:
+    global import_module_exist
+    if not import_module_exist:
         logging.info('Simulating motion detection for test purposes')
         event_detected('P8_11')
     return 'Processed bbb_io'
