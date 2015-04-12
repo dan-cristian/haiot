b1a0983a2876bc281e89c0d50630ee4dc7bda1f5
dcristian <dan.cristian@gmail.com>
2015-04-12 09:30:39 +0300

    sudo err detect

--- startserver_mem.sh
+++ startserver_mem.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 OUT_FILE=/tmp/iot-nohup.out
-mv $OUT_FILE $OUT_FILE.last
+mv -f -v $OUT_FILE $OUT_FILE.last
 nohup ./startserver.sh db_mem model_auto_update > $OUT_FILE &
 echo Tailing log file, you can exit safely with CTRL+C, program will continue to run
 tail -f $OUT_FILE
