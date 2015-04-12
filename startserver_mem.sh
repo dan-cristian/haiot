7b398991ef0b6557b04f8999878906d76515ef52
dcristian <dan.cristian@gmail.com>
2015-04-12 09:26:41 +0300

    sudo err detect

--- startserver_mem.sh
+++ startserver_mem.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-nohup ./startserver.sh db_mem model_auto_update > /tmp/iot-nohup.out &
+OUT_FILE = /tmp/iot-nohup.out
+nohup ./startserver.sh db_mem model_auto_update > $OUT_FILE &
+
+mv $OUT_FILE $OUT_FILE.last
