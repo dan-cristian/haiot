59098d53435ad9f012a495a134592fccf42feb36
dcristian <dan.cristian@gmail.com>
2015-04-19 22:02:50 +0300

    startup

--- startserver_daemon.sh
+++ startserver_daemon.sh
@@ -3,4 +3,5 @@
 OUT_FILE=/tmp/iot-nohup.out
 #mv -f -v $OUT_FILE $OUT_FILE.last
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-$DIR/startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 log=$OUT_FILE $1 $2 $3 $4 $5 > $OUT_FILE 2>&1 &
+$DIR/startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 $1 $2 $3 $4 $5 >> $OUT_FILE 2>&1
+echo "haiot startserver daemon exit" >> $OUT_FILE
