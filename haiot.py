e609c95868fe2fd116cee4e1f3653dc19840890e
dcristian <dan.cristian@gmail.com>
2015-04-19 21:02:57 +0300

    startup

--- haiot.py
+++ haiot.py
@@ -3,8 +3,10 @@ __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
 import sys
 import main
+import os
 
 #http://www.tutorialspoint.com/python/python_command_line_arguments.htm
 
 if __name__ == '__main__':
+    os.system('/bin/bash  --rcfile /home/dcristian/PYC/venv/bin/activate')
     main.run(sys.argv[1:])
