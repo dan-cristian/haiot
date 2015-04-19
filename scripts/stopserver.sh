ba2a17f76b0463547104183c6f6e307515ad4e8c
dcristian <dan.cristian@gmail.com>
2015-04-19 22:10:25 +0300

    startup

--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -2,15 +2,15 @@
 proc_pid=$(ps w | grep '[h]aiot.py' | awk '{print $1}')
 echo "proc pid=$proc_pid, 1st attempt"
 if [ -z "$proc_pid" ]; then
-    echo No pid found with ps w, trying now with ps wx
+    echo "No pid found with ps w, trying now with ps wx"
     proc_pid=$(ps wx | grep '[h]aiot.py' | awk '{print $1}')
     echo "proc pid=$proc_pid, 2nd attempt"
 fi
 if [ -n "$proc_pid" ]; then
-    echo Killing proc id [$proc_pid]
+    echo "Killing proc id [$proc_pid]"
     kill $proc_pid
     sleep 1
     kill -9 $proc_pid
 else
-    echo Program is not running, nothing to stop
+    echo "Program is not running, nothing to stop"
 fi
