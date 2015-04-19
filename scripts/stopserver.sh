0344b71076c285de588b28d10052716a3ca949cd
dcristian <dan.cristian@gmail.com>
2015-04-19 22:12:41 +0300

    startup

--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-export proc_pid=$(ps w | grep '[h]aiot.py' | awk '{print $1}')
+proc_pid=$(ps w | grep '[h]aiot.py' | awk '{print $1}')
 echo "proc pid=$proc_pid, 1st attempt"
 if [ -z "$proc_pid" ]; then
     echo "No pid found with ps w, trying now with ps wx"
-    export proc_pid=$(ps wx | grep '[h]aiot.py' | awk '{print $1}')
+    proc_pid=$(ps wxa | grep '[h]aiot.py' | awk '{print $1}')
     echo "proc pid=$proc_pid, 2nd attempt"
 fi
 if [ -n "$proc_pid" ]; then
