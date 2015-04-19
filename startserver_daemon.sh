b317098345dda247b677a2600c18e5fb28accbfc
dcristian <dan.cristian@gmail.com>
2015-04-19 21:56:39 +0300

    startup

--- startserver_daemon.sh
+++ startserver_daemon.sh
@@ -3,4 +3,4 @@
 OUT_FILE=/tmp/iot-nohup.out
 #mv -f -v $OUT_FILE $OUT_FILE.last
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-$DIR/startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 log=$OUT_FILE $1 $2 $3 $4 $5 > $OUT_FILE 2>&1
+$DIR/startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 log=$OUT_FILE $1 $2 $3 $4 $5 > $OUT_FILE 2>&1 &
