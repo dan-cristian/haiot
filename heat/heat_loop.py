ef15cd01adf606c36476d302fe685f74a477170b
dcristian <dan.cristian@gmail.com>
2015-04-19 15:24:59 +0300

    change log to syslog

--- heat/heat_loop.py
+++ heat/heat_loop.py
@@ -38,39 +38,45 @@ def __decide_action(zone, current_temperature, target_temperature):
     __save_heat_state_db(zone=zone, heat_is_on=heat_is_on)
 
 def __update_zone_heat(zone, heat_schedule, sensor):
-    minute = datetime.datetime.now().minute
-    hour = datetime.datetime.now().hour
-    weekday = datetime.datetime.today().weekday()
-    if weekday <= 5:
-        schedule_pattern=models.SchedulePattern.query.filter_by(id=heat_schedule.pattern_week_id).first()
-    else:
-        schedule_pattern=models.SchedulePattern.query.filter_by(id=heat_schedule.pattern_weekend_id).first()
-    if schedule_pattern:
-        pattern = schedule_pattern.pattern
-        temperature_code = pattern[hour]
-        temperature = models.TemperatureTarget.query.filter_by(code=temperature_code).first()
-        if temperature:
-            logger.info('Active pattern for zone {} is {} temp {}'.format(zone.name, schedule_pattern.name,
-                                                                                 temperature.target))
-            if sensor.temperature:
-                __decide_action(zone, sensor.temperature, temperature.target)
+    try:
+        minute = datetime.datetime.now().minute
+        hour = datetime.datetime.now().hour
+        weekday = datetime.datetime.today().weekday()
+        if weekday <= 5:
+            schedule_pattern=models.SchedulePattern.query.filter_by(id=heat_schedule.pattern_week_id).first()
         else:
-            logger.critical('Unknown temperature pattern code {}'.format(temperature_code))
+            schedule_pattern=models.SchedulePattern.query.filter_by(id=heat_schedule.pattern_weekend_id).first()
+        if schedule_pattern:
+            pattern = schedule_pattern.pattern
+            temperature_code = pattern[hour]
+            temperature = models.TemperatureTarget.query.filter_by(code=temperature_code).first()
+            if temperature:
+                logger.info('Active pattern for zone {} is {} temp {}'.format(zone.name, schedule_pattern.name,
+                                                                                     temperature.target))
+                if sensor.temperature:
+                    __decide_action(zone, sensor.temperature, temperature.target)
+            else:
+                logger.critical('Unknown temperature pattern code {}'.format(temperature_code))
+    except Exception, ex:
+        logger.error('Error updatezoneheat, err={}'.format(ex, exc_info=True))
 
 def loop_zones():
-    zone_list = models.Zone.query.all()
-    global progress_status
-    for zone in zone_list:
-        progress_status = 'do zone {}'.format(zone.name)
-        heat_schedule = models.HeatSchedule.query.filter_by(zone_id=zone.id).first()
-        zonesensor = models.ZoneSensor.query.filter_by(zone_id=zone.id).first()
-        if heat_schedule and zonesensor:
-            sensor = models.Sensor.query.filter_by(address=zonesensor.sensor_address).first()
-            if heat_schedule.active and sensor:
-                sensor_last_update_seconds = (datetime.datetime.now()-sensor.updated_on).total_seconds()
-                if sensor_last_update_seconds > 60 * 60:
-                    logger.warning('Sensor {} not updated in last 60 minutes, unusual'.format(sensor.zone_name))
-                __update_zone_heat(zone, heat_schedule, sensor)
+    try:
+        zone_list = models.Zone.query.all()
+        global progress_status
+        for zone in zone_list:
+            progress_status = 'do zone {}'.format(zone.name)
+            heat_schedule = models.HeatSchedule.query.filter_by(zone_id=zone.id).first()
+            zonesensor = models.ZoneSensor.query.filter_by(zone_id=zone.id).first()
+            if heat_schedule and zonesensor:
+                sensor = models.Sensor.query.filter_by(address=zonesensor.sensor_address).first()
+                if heat_schedule.active and sensor:
+                    sensor_last_update_seconds = (datetime.datetime.now()-sensor.updated_on).total_seconds()
+                    if sensor_last_update_seconds > 60 * 60:
+                        logger.warning('Sensor {} not updated in last 60 minutes, unusual'.format(sensor.zone_name))
+                    __update_zone_heat(zone, heat_schedule, sensor)
+    except Exception, ex:
+        logger.error('Error loop_zones, err={}'.format(ex, exc_info=True))
 
 progress_status = None
 def get_progress():
