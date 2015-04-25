97e91d61ff4ec6eaaa50213b6fbb3b49f27e125b
Dan Cristian <dan.cristian@gmail.com>
2015-04-25 17:29:03 +0300

    ease startup

--- startserver_daemon.sh
+++ startserver_daemon.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+function log(){
+	logger -i -t sync $1
+	echo $1
+}
 
+log "Executing script with parameter $1 $2 $3"
 OUT_FILE=/tmp/iot-nohup.out
 #mv -f -v $OUT_FILE $OUT_FILE.last
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo "Current dir for daemon is $DIR"
-$DIR/startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 live $1 $2 $3 $4 $5 >> $OUT_FILE 2>&1
-echo "Haiot startserver daemon exit" >> $OUT_FILE
+DIR="$(dirname "$(readlink -f "$0")")"
+#$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+log "Current dir for haiot daemon is $DIR"
+$DIR/startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 live $1 $2 $3 $4 $5  2>&1 | logger -i -t sync
+log "Haiot startserver daemon exit" >> $OUT_FILE
