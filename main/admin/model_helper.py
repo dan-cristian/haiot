473aea75cc27c95a71c8dd196d1195c3ca2af8f9
dcristian <dan.cristian@gmail.com>
2015-05-02 18:09:04 +0300

    relay heat

--- main/admin/model_helper.py
+++ main/admin/model_helper.py
@@ -316,9 +316,12 @@ def populate_tables(model_auto_update=False):
 
     check_table_schema(models.ZoneHeatRelay, model_auto_update)
     #fixme: mapping not correct
-    heat_relay_list={'pi-power': [[19, '24']],
-                     #1=bucatarie, 2=living, 47=birou, 4=dormitor
-                     'beaglebone': [[1,'P9_11'],[2,'P9_12'],[47,'P9_13'],[4,'P8_15']]}
+    heat_relay_list={
+        #19=heat main
+        'pi-power': [[19, '24']],
+        #1=bucatarie, 2=living, 47=birou, 4=dormitor
+        'beaglebone': [[1,'P9_11'],[2,'P9_12'],[47,'P9_13'],[4,'P8_15']]
+    }
     #,[4,'P8_16']
     if len(models.ZoneHeatRelay.query.all()) < len(heat_relay_list):
         models.ZoneHeatRelay.query.delete()
