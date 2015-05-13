c96e86f22cf39fcf792eee11894d2136ccd75450
dcristian <dan.cristian@gmail.com>
2015-05-13 21:22:36 +0300

    mqtt and plotly rework for openshift

--- transport/mqtt_io/__init__.py
+++ transport/mqtt_io/__init__.py
@@ -92,8 +92,8 @@ def init():
         host_list=[
             [model_helper.get_param(constant.P_MQTT_HOST_3), int(model_helper.get_param(constant.P_MQTT_PORT_3))],
             [model_helper.get_param(constant.P_MQTT_HOST_1), int(model_helper.get_param(constant.P_MQTT_PORT_1))],
-            [model_helper.get_param(constant.P_MQTT_HOST_2), int(model_helper.get_param(constant.P_MQTT_PORT_2))],
-            [model_helper.get_param(constant.P_MQTT_HOST_3), int(model_helper.get_param(constant.P_MQTT_PORT_3))]
+            [model_helper.get_param(constant.P_MQTT_HOST_2), int(model_helper.get_param(constant.P_MQTT_PORT_2))]
+            #[model_helper.get_param(constant.P_MQTT_HOST_3), int(model_helper.get_param(constant.P_MQTT_PORT_3))]
             ]
         global topic
         topic=str(model_helper.get_param(constant.P_MQTT_TOPIC))
