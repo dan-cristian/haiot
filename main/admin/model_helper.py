915d310976aae4473c357ce45134375275d37d9a
dcristian <dan.cristian@gmail.com>
2015-05-25 23:51:42 +0300

    ups serial

--- main/admin/model_helper.py
+++ main/admin/model_helper.py
@@ -236,6 +236,7 @@ def populate_tables(model_auto_update=False):
     check_table_schema(models.SystemMonitor, model_auto_update)
     check_table_schema(models.SystemDisk, model_auto_update)
     check_table_schema(models.Sensor, model_auto_update)
+    check_table_schema(models.Ups, model_auto_update)
     #if len(models.Sensor.query.all()) == 0:
         #logger.info('Populating Sensor with a test value')
         #sens = models.Sensor(address='ADDRESSTEST')
