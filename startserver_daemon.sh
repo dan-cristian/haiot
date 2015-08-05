67874cdda65378aedb137e33570f57769a8754b6
dcristian <dan.cristian@gmail.com>
2015-08-05 19:04:19 +0300

    refactor db init

--- startserver_daemon.sh
+++ startserver_daemon.sh
@@ -10,14 +10,16 @@ OUT_FILE=/tmp/iot-nohup.out
 #$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 log "Current dir for haiot daemon is $DIR. Pause for 10 seconds to allow network interfaces to start."
 sleep 10
-$DIR/startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 live $1 $2 $3 $4 $5  2>&1 | logger -t haiot
+cd $DIR
+./startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 live $1 $2 $3 $4 $5  2>&1 | logger -t haiot
 log "Haiot startserver daemon exit" >> $OUT_FILE
 }
 
 stop() {
 	me=`basename $0`
-        log "Stopping script $me"
-        $DIR/scripts/stopserver.sh
+	log "Stopping script $me"
+    cd $DIR
+    ./scripts/stopserver.sh
 }
 
 log "Executing script with parameter $1 $2 $3"
