503a784734ef2f29ada2025efe1cefc8e4236ddd
dcristian <dan.cristian@gmail.com>
2015-08-17 00:02:49 +0300

    scheduler & plotly disable

--- rules/__init__.py
+++ rules/__init__.py
@@ -15,26 +15,28 @@ def parse_rules(obj, change):
     global __func_list
     #executed on all db value changes
     logger.debug('Received obj={} change={} for rule parsing'.format(obj, change))
-    #extract only changed fields
-    if hasattr(obj,'last_commit_field_changed_list'):
-        field_changed_list = obj.last_commit_field_changed_list
-    else:
-        field_changed_list = []
-    for func in __func_list:
-        if len(func[1].func_defaults) > 0:
-            first_param = func[1].func_defaults[0]
-            #calling rule methods with first param type equal to passed object type
-            if type(obj) == type(first_param):
-                result = getattr(rules, func[0])(obj=obj, field_changed_list=field_changed_list)
-                logger.debug('Rule returned {}'.format(result))
-
+    try:
+        #extract only changed fields
+        if hasattr(obj,'last_commit_field_changed_list'):
+            field_changed_list = obj.last_commit_field_changed_list
+        else:
+            field_changed_list = []
+        for func in __func_list:
+            if func[1].func_defaults and len(func[1].func_defaults) > 0:
+                first_param = func[1].func_defaults[0]
+                #calling rule methods with first param type equal to passed object type
+                if type(obj) == type(first_param):
+                    result = getattr(rules, func[0])(obj=obj, field_changed_list=field_changed_list)
+                    logger.debug('Rule returned {}'.format(result))
+    except Exception, ex:
+        logger.critical('Error parsing rules: {}', format(ex))
 
 def thread_run():
     logger.debug('Processing template_run')
     return 'Processed template_run'
 
 def unload():
-    logger.info('Template module unloading')
+    logger.info('Rules module unloading')
     #...
     thread_pool.remove_callable(rules.thread_run)
     global initialised
