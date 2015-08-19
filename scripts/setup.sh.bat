67aac1eb0e12c6805b6f1b7c40287b41cd8814b3
dcristian <dan.cristian@gmail.com>
2015-08-19 23:10:32 +0300

    fix install

--- scripts/setup.sh.bat
+++ scripts/setup.sh.bat
@@ -2,6 +2,7 @@
 sudo pip install virtualenv
 virtualenv venv
 source venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 chmod +x startserver*.sh
 chmod +x scripts/stopserver.sh
