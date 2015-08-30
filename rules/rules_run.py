fd63d57c0da4af8535e1da768354fd760071ac7f
dcristian <dan.cristian@gmail.com>
2015-08-30 09:49:00 +0300

    rules

--- rules/rules_run.py
+++ rules/rules_run.py
@@ -26,36 +26,68 @@ def rule_sensor_temp_target(obj = models.Sensor(), field_changed_list = []):
 
 ######## CRON RULES ################
 try:
-    @scheduler.scheduled_job('cron', day='*', hour='8', minute='0', second='0')
+    @scheduler.scheduled_job('cron', day='*', hour='9', minute='55', second='0')
     def rule_water_front_on(): do_job(water_front_on)
 
-    @scheduler.scheduled_job('cron', day='*', hour='8', minute='5', second='0')
+    @scheduler.scheduled_job('cron', day='*', hour='9', minute='56', second='0')
     def rule_water_front_off(): do_job(water_front_off)
+
+    @scheduler.scheduled_job('cron', day='*', hour='9', minute='57', second='0')
+    def rule_water_front_on(): do_job(water_back_on)
+
+    @scheduler.scheduled_job('cron', day='*', hour='9', minute='58', second='0')
+    def rule_water_front_off(): do_job(water_back_off)
+
+
 except Exception, ex:
     logger.error('Unable to initialise apscheduler based rules, err={}'.format(ex))
 
 ###### JOBS executed asyncronously via a thread pool ######
 
-def water_front_on():
-    logger.info('water on')
-    with app.test_client() as c:
-        msg = c.get('/apiv1/db_update/model_name=ZoneCustomRelay&'
-                    'filter_name=relay_pin_name&filter_value=back pump relay&field_name=relay_is_on&field_value=1').data
-        logger.info(msg)
+def back_pump_on():
+    logger.info('back pump on')
+    __update_custom_relay('back pump relay',1)
     #with app.test_request_context():
     #    logger.info(redirect('/apiv1/relay/get'))
     # start the pump
     # open valve
 
+def back_pump_off():
+    logger.info('back pump off')
+    __update_custom_relay('back pump relay',0)
+
+
+def water_front_on():
+    logger.info('water front on')
+    back_pump_on()
+    __update_custom_relay('front valve relay',1)
+
 
 def water_front_off():
-    logger.info('water off')
-    # close the valve
+    logger.info('water front off')
+    __update_custom_relay('front valve relay',0)
     # let the pump build some pressure
-    time.sleep(10)
+    time.sleep(5)
     # pump off if no other zone is on?
+    back_pump_off()
+
+def water_back_on():
+    logger.info('water back on')
+    back_pump_on()
+    __update_custom_relay('back valve relay',1)
+
+
+def water_back_off():
+    logger.info('water back off')
+    __update_custom_relay('back valve relay',0)
+    # let the pump build some pressure
+    time.sleep(5)
+    # pump off if no other zone is on?
+    back_pump_off()
+
+def __update_custom_relay(relay_pin_name, power_is_on):
     with app.test_client() as c:
         msg = c.get('/apiv1/db_update/model_name=ZoneCustomRelay&'
-                    'filter_name=relay_pin_name&filter_value=back pump relay&field_name=relay_is_on&field_value=0').data
-        logger.info(msg)
-
+                'filter_name=relay_pin_name&filter_value={}&field_name=relay_is_on&field_value={}'.
+                format(relay_pin_name, power_is_on)).data
+    logger.info(msg)
