49dc29f3a21ae2b2fde6297b8ec1b51d5a5baab5
dcristian <dan.cristian@gmail.com>
2015-10-18 14:01:13 +0300

    fix delete by mistake, added uanet

---
+++ haiot.py
@@ -0,0 +1,8 @@
+__author__ = 'Dan Cristian<dan.cristian@gmail.com>'
+import sys
+
+#http://www.tutorialspoint.com/python/python_command_line_arguments.htm
+
+if __name__ == '__main__':
+    import main
+    main.run(sys.argv[1:])
---
+++ startserver.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#echo Enter mem[Enter] to run db in memory. Default is from disk
+#read -t 3 answer
+#if [ $? == 0 ]; then
+#    echo "Selected"
+#else
+#    echo "Can't wait anymore!"
+#answer=disk
+#fi
+#echo "Your answer is: $answer"
+function run_app {
+    $DIR/scripts/stopserver.sh
+    sleep 2
+    echo Starting app with parameter $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
+    source $DIR/venv/bin/activate
+    python $DIR/haiot.py $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
+    exit_code=$?
+    echo "Program exit with code $exit_code"
+    echo "---------------------------------"
+}
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "Current dir on start is $DIR"
+must_run=true
+echo "Getting latest version from git"
+git pull --no-edit
+while $must_run; do
+    run_app $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
+    if [ $exit_code == 131 ]; then
+        echo "Restarting app"
+    fi
+    if [ $exit_code == 132 ]; then
+        echo "Upgrading app"
+        cd $DIR
+        git pull --no-edit
+    fi
+    if [ $exit_code == 133 ]; then
+        echo "Shutdown app"
+        must_run=false
+    fi
+    if [ $exit_code == 143 ]; then
+        echo "App was killed"
+        must_run=false
+    fi
+    if [ $exit_code == 137 ]; then
+        echo "App was killed with -9"
+        must_run=false
+    fi
+    if [ $exit_code == 1 ]; then
+        echo "App was interrupted with CTRL-C or by exception code[$exit_code]"
+        must_run=false
+    fi
+done
