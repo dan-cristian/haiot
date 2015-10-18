6ebeb744b4d3bf756ed597a81a2d11ea1a8eb268
dcristian <dan.cristian@gmail.com>
2015-10-18 16:05:25 +0300

    userspaces init

--- scripts/osinstall/pi/post-install.sh
+++ scripts/osinstall/pi/post-install.sh
@@ -11,7 +11,7 @@ echo "Updating apt-get"
 apt-get upgrade
 apt-get update
 echo "Installing additional packages"
-apt-get -y install sudo apt-utils mc nano locales python wget owfs git python-rpi.gpio inotify-tools
+apt-get -y install sudo apt-utils mc nano locales python wget owfs git python-rpi.gpio inotify-tools killall
 
 echo "Installing python pip and virtualenv"
 wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
@@ -22,6 +22,7 @@ pip install virtualenv
 echo Creating user $USERNAME with password=$USERPASS
 useradd $USERNAME -m
 echo "$USERNAME:$USERPASS" | chpasswd
+adduser $USERNAME sudo
 
 echo Getting HAIOT application from github
 cd /home/$USERNAME
@@ -30,6 +31,7 @@ git clone http://192.168.0.9:888/PYC.git
 echo Configuring HAIOT application
 cd PYC 
 chmod +x scripts/*sh*
+chmod +x *.sh
 scripts/setup.sh.bat
 chown -R $USERNAME:$USERNAME .
 
@@ -44,8 +46,8 @@ echo Testing init service, create working directories for all defined linux user
 /etc/init.d/userspaceServices start
 /etc/init.d/userspaceServices stop
 echo Creating start links for haiot to be picked up by userspaceServices
-ln -s /home/$USERNAME/PYC/startserver_daemon.sh /home/$USERNAME/.startUp/
-ln -s /home/$USERNAME/PYC/startserver_daemon.sh /home/$USERNAME/.shutDown/
+ln -s /home/$USERNAME/PYC/start_daemon_userspaces.sh /home/$USERNAME/.startUp/
+ln -s /home/$USERNAME/PYC/start_daemon_userspaces.sh /home/$USERNAME/.shutDown/
 chown -R $USERNAME:$USERNAME /home/$USERNAME/
 
 echo Starting haiot via userspaceServices
