3590d2bdb97480f3f4d9e9b13a40363c9ac6ccb3
dcristian <dan.cristian@gmail.com>
2015-10-18 14:57:18 +0300

    userspaces init

--- scripts/osinstall/pi/post-install.sh
+++ scripts/osinstall/pi/post-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+USERNAME=haiot
+USERPASS=haiot
+
 echo "Setting timezone ..."
 echo "Europe/Bucharest" > /etc/timezone 
 dpkg-reconfigure -f noninteractive tzdata
@@ -8,7 +11,7 @@ echo "Updating apt-get"
 apt-get upgrade
 apt-get update
 echo "Installing additional packages"
-apt-get -y install sudo apt-utils mc nano locales rsync python wget owfs git python-rpi.gpio inotify-tools
+apt-get -y install sudo apt-utils mc nano locales python wget owfs git python-rpi.gpio inotify-tools
 
 echo "Installing python pip and virtualenv"
 wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
@@ -16,21 +19,37 @@ python get-pip.py
 rm get-pip.py
 pip install virtualenv
 
-echo Creating user haiot with password=haiot
-useradd haiot -m
-echo "haiot:haiot" | chpasswd
-#passwd haiot
+echo Creating user $USERNAME with password=$USERPASS
+useradd $USERNAME -m
+echo "$USERNAME:$USERPASS" | chpasswd
 
-echo Getting HAIOT application
-cd /home/haiot
+echo Getting HAIOT application from github
+cd /home/$USERNAME
 git clone http://192.168.0.9:888/PYC.git
 
 echo Configuring HAIOT application
 cd PYC 
 chmod +x scripts/*sh*
 scripts/setup.sh.bat
-chown -R haiot:haiot .
-#todo init scripts
+chown -R $USERNAME:$USERNAME .
+
+echo Downloading init service
+cd ~
+wget https://raw.githubusercontent.com/ivankovacevic/userspaceServices/master/userspaceServices
+chmod +x userspaceServices
+echo Installing init service
+mv userspaceServices /etc/init.d/
+update-rc.d userspaceServices defaults
+echo Testing init service, create working directories for all defined linux users incl. haiot
+/etc/init.d/userspaceServices start
+/etc/init.d/userspaceServices stop
+echo Creating start links for haiot to be picked up by userspaceServices
+ln -s /home/$USERNAME/PYC/startserver_daemon.sh /home/$USERNAME/.startUp/
+ln -s /home/$USERNAME/PYC/startserver_daemon.sh /home/$USERNAME/.shutDown/
+chown -R $USERNAME:$USERNAME /home/$USERNAME/
+
+echo Starting haiot via userspaceServices
+/etc/init.d/userspaceServices restart
 
 echo "Removing not needed files and cleaning apt files"
 rm /usr/share/doc -r
--- startserver_daemon.sh
+++ startserver_daemon.sh
@@ -5,14 +5,14 @@ function log(){
 }
 
 start(){
-OUT_FILE=/tmp/iot-nohup.out
+#OUT_FILE=/tmp/iot-nohup.out
 #mv -f -v $OUT_FILE $OUT_FILE.last
 #$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 log "Current dir for haiot daemon is $DIR. Pause for 10 seconds to allow network interfaces to start."
 sleep 10
 cd $DIR
 ./startserver.sh db_mem model_auto_update syslog=logs2.papertrailapp.com:30445 $1 $2 $3 $4 $5  2>&1 | logger -t haiot
-log "Haiot startserver daemon exit" >> $OUT_FILE
+log "Haiot startserver daemon exit" # >> $OUT_FILE
 }
 
 stop() {
