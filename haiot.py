d1cdd3aeb1147cbfd113f32a1198d3838ab0e862
dcristian <dan.cristian@gmail.com>
2015-10-18 13:49:26 +0300

    NETBOOK dcristian

--- haiot.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Dan Cristian<dan.cristian@gmail.com>'
-import sys
-
-#http://www.tutorialspoint.com/python/python_command_line_arguments.htm
-
-if __name__ == '__main__':
-    import main
-    main.run(sys.argv[1:])
