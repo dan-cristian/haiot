e0a1339f2daf9de1e32864eb044ba992dc3dd8ba
dcristian <dan.cristian@gmail.com>
2015-10-18 23:25:56 +0300

    init

--- scripts/setup.sh.bat
+++ scripts/setup.sh.bat
@@ -15,3 +15,12 @@ pip install -r requirements.txt
 pip install -r requirements-beaglebone.txt
 pip install -r requirements-rpi.txt
 pip install -r requirements-win.txt
+
+# installing mysql connector
+wget http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.3.zip
+unzip mysql-connector-python-2.1.3.zip
+cd mysql-connector-python-2.1.3/
+python setup.py install
+cd ..
+rm mysql-connector-python-2.1.3.zip
+rm -r mysql-connector-python-2.1.3
