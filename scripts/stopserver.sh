f6138ef54a95471765ab9bfec48a949cb165f082
dcristian <dan.cristian@gmail.com>
2015-10-18 22:39:02 +0300

    init

--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -10,7 +10,7 @@ if [ -n "$proc_pid" ]; then
     echo "Killing proc id [$proc_pid]"
     kill $proc_pid
     sleep 2
-    kill -9 $proc_pid > /dev/nul 2>&1
+    kill -9 $proc_pid > /dev/null 2>&1
 else
     echo "Program is not running or no pid found, nothing to stop"
 fi
