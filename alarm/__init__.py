d5389807fbf107bbf863cce79393aa4bd213a27f
dcristian <dan.cristian@gmail.com>
2015-10-21 00:35:17 +0300

    PiGpio support

--- alarm/__init__.py
+++ alarm/__init__.py
@@ -40,8 +40,8 @@ def init():
     port_list = []
     local_alarms = models.ZoneAlarm().query_filter_all(models.ZoneAlarm.gpio_host_name.in_([Constant.HOST_NAME]))
     for alarm in local_alarms:
-        gpio_pin = models.GpioPin().query_filter_first(models.ZoneAlarm.gpio_pin_code.in_([alarm.gpio_pin_code]),
-                                                     models.ZoneAlarm.gpio_host_name.in_([Constant.HOST_NAME]))
+        gpio_pin = models.GpioPin().query_filter_first(models.GpioPin.pin_code.in_([alarm.gpio_pin_code]),
+                                                     models.GpioPin.host_name.in_([Constant.HOST_NAME]))
         if gpio_pin:
             Log.logger.info('Setting up alarm port pin={} type={}'.format(gpio_pin.pin_code, gpio_pin.pin_type))
             port_list.append(gpio_pin)
