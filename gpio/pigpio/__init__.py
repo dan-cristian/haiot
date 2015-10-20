87f27971b28b1b03c8c64ab8c4ddb29daece7ed2
dcristian <dan.cristian@gmail.com>
2015-10-21 00:41:15 +0300

    PiGpio support

--- gpio/pigpio/__init__.py
+++ gpio/pigpio/__init__.py
@@ -44,7 +44,8 @@ def setup_in_ports(gpio_pin_list):
     if __pi:
         for gpio_pin in gpio_pin_list:
             if gpio_pin.pin_type == Constant.GPIO_PIN_TYPE_PI_STDGPIO:
-                Log.logger.info('Set pin {} type {} as input'.format(gpio_pin.pin_code, gpio_pin.pin_type))
+                Log.logger.info('Set pin={} type={} index={} as input'.format(gpio_pin.pin_code, gpio_pin.pin_index_bcm,
+                                                                              gpio_pin.pin_type))
                 __pi.set_mode(gpio_pin.pin_index_bcm, pigpio.INPUT)
                 __callback.append(__pi.callback(user_gpio=gpio_pin.pin_index_bcm,
                                                 edge=pigpio.EITHER_EDGE, func=input_event))
