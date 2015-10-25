24d2ef386bc4b77ae701ce14ffab4129fa06f5c1
dcristian <dan.cristian@gmail.com>
2015-10-25 22:00:19 +0200

    in bbb alarm improvs

--- webui/__init__.py
+++ webui/__init__.py
@@ -8,8 +8,44 @@ from main.admin import model_helper
 from common import Constant
 import helpers
 
-initialised=False
-flask_thread=None
+initialised = False
+flask_thread = None
+
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    http://flask.pocoo.org/snippets/35/
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
 
 @app.route('/exit', methods=['POST'])
 def exit():
@@ -51,6 +87,7 @@ def init():
         #otherwise listen on all interfaces
         host='0.0.0.0'
         port = model_helper.get_param(Constant.P_FLASK_WEB_PORT)
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
     flask_thread = helpers.FlaskInThread(app, host=host, port=port, debug=True, use_reloader=False)
     initialised = True
     flask_thread.start()
