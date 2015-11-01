0b366da1d12a5024fcd23f79d356ce7a45bb2693
dcristian <dan.cristian@gmail.com>
2015-11-02 00:06:07 +0200

    fusepy

--- scripts/osinstall/debian/post-install.sh
+++ scripts/osinstall/debian/post-install.sh
@@ -23,11 +23,36 @@ python get-pip.py
 rm get-pip.py
 pip install --no-cache-dir virtualenv
 
+
 echo "Creating user $USERNAME with password=$USERPASS"
 useradd $USERNAME -m
 echo "$USERNAME:$USERPASS" | chpasswd
 adduser $USERNAME sudo
 chsh -s /bin/bash $USERNAME
+
+echo "Installing duplicity and hubic"
+# http://www.yvangodard.me/hubic-backup-duplicity-backend-pyrax/
+# https://www.tiernanotoole.ie/2015/04/01/Duplicity_Hubic.html
+apt-get -y purge duplicity
+# http://serverfault.com/questions/22414/how-can-i-run-debian-stable-but-install-some-packages-from-testing
+echo "Package: *
+Pin: release a=unstable
+Pin-Priority: 50" > /etc/apt/preferences.d/unstable.pref
+echo "deb     http://ftp.us.debian.org/debian/    unstable main contrib non-free" > /etc/apt/sources.list.d/unstable.list
+apt-get update
+apt-get -y -t unstable install duplicity
+echo "Installing pyrax prerequisite for hubic support"
+pip install --no-cache-dir --index-url=http://pypi.python.org/simple/ --trusted-host pypi.python.org  setuptools-scm
+apt-get -y install build-essential python-dev
+pip install --no-cache-dir pyrax
+echo "[hubic]
+email = <hubicemailaddress>
+password = <hubicpassword>
+client_id = <hubicclientid>
+client_secret = <hubicclientsecret>
+redirect_uri = http://localhost/" > /home/$USERNAME/.hubic_credentials
+chown $USERNAME /home/$USERNAME/.hubic_credentials
+chmod 700 /home/$USERNAME/.hubic_credentials
 
 echo "Getting HAIOT application from github"
 cd /home/$USERNAME
@@ -61,8 +86,11 @@ chown -R $USERNAME:$USERNAME /home/$USERNAME/
 echo "Starting haiot via userspaceServices"
 /etc/init.d/userspaceServices restart
 
+
+
+
 echo "Removing not needed files and cleaning apt files"
-apt-get -y remove build-essential
+apt-get -y remove build-essential python-dev
 rm /usr/share/doc -r
 rm /usr/share/man -r
 apt-get -y autoremove
