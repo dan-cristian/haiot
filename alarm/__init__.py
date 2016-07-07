e7746740afef768f323de649e447e801c1344b87
Dan Cristian <dan.cristian@gmail.com>
2016-07-07 23:46:11 +0300

    alarm contact

--- alarm/__init__.py
+++ alarm/__init__.py
@@ -34,20 +34,23 @@ def unload():
 
 def init():
     Log.logger.info('Alarm module initialising')
-    #alarm_loop.init()
-    #thread_pool.add_interval_callable(alarm_loop.thread_run)
+    # alarm_loop.init()
+    # thread_pool.add_interval_callable(alarm_loop.thread_run)
     dispatcher.connect(handle_event_alarm, signal=Constant.SIGNAL_GPIO, sender=dispatcher.Any)
     # get list of input gpio ports and communicate them to gpio modules for proper port setup as "IN"
     port_list = []
     local_alarms = models.ZoneAlarm().query_filter_all(models.ZoneAlarm.gpio_host_name.in_([Constant.HOST_NAME]))
     for alarm in local_alarms:
         gpio_pin = models.GpioPin().query_filter_first(models.GpioPin.pin_code.in_([alarm.gpio_pin_code]),
-                                                     models.GpioPin.host_name.in_([Constant.HOST_NAME]))
+                                                       models.GpioPin.host_name.in_([Constant.HOST_NAME]))
         if gpio_pin:
             Log.logger.info('Schedule setup alarm port pin={} type={}'.format(gpio_pin.pin_code, gpio_pin.pin_type))
             port_list.append(gpio_pin)
         else:
             Log.logger.warning('Unexpected empty gpio pin response for alarm setup')
     dispatcher.send(signal=Constant.SIGNAL_GPIO_INPUT_PORT_LIST, gpio_pin_list=port_list)
+    # just for test on netbook
+    # import gpio.rpi_gpio
+    # gpio.rpi_gpio.setup_in_ports(port_list)
     global initialised
     initialised = True
