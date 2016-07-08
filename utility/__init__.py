a7371ed9ab213671244cb1c421eb293d184e1d3e
Dan Cristian <dan.cristian@gmail.com>
2016-07-08 08:17:37 +0300

    utility change

--- utility/__init__.py
+++ utility/__init__.py
@@ -19,11 +19,13 @@ def __utility_update(sensor_name, units_delta_a, units_delta_b, total_units_a, t
             if current_record:
                 record.sensor_index = index
                 record.units_delta = delta / (current_record.ticks_per_unit * 1.0)  # force float operation
+                record.ticks_delta = delta
                 if not current_record.units_total:
                     current_record.units_total = 0.0
                 # force save for history recording
                 if current_record:
                     current_record.units_delta = 0.0
+                    current_record.ticks_delta = 0
                 record.units_total = 0.0 + current_record.units_total + record.units_delta
                 record.save_changed_fields(current_record=current_record, new_record=record,
                                            notify_transport_enabled=True, save_to_graph=True)
