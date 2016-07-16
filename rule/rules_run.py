d776cf8d233f76c137e3858e22b5f0763f50fcb7
Dan Cristian <dan.cristian@gmail.com>
2016-07-16 23:04:57 +0300

    rule bell

--- rule/rules_run.py
+++ rule/rules_run.py
@@ -54,6 +54,8 @@ def rule_alarm(obj=models.ZoneAlarm(), field_changed_list=None):
     # Log.logger.info('Rule Alarm: obj={} fields={}'.format(obj, field_changed_list))
     if obj.alarm_status == 1:
         Log.logger.info('Rule Alarm ON:  pin={} status={}'.format(obj.alarm_pin_name, obj.alarm_status))
+        if obj.alarm_pin_name == 'sonerie':
+            rule_common.play_bell_local()
     else:
         Log.logger.info('Rule Alarm OFF: pin={} status={}'.format(obj.alarm_pin_name, obj.alarm_status))
     return 'zone alarm ok'
