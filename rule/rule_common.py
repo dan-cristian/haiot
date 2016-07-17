37cc5be4d1c9f5d05918cc04cff6dbd8e04b5633
Dan Cristian <dan.cristian@gmail.com>
2016-07-17 09:42:39 +0300

    add newtifry

--- rule/rule_common.py
+++ rule/rule_common.py
@@ -37,5 +37,5 @@ def play_bell_local(sound_file):
     pass
 
 
-def send_notification(title, message, url, priority, deviceid, image_url):
+def send_notification(title='Default', message=None, url=None, priority=None, deviceid=None, image_url=None):
     dispatcher.send(Constant.SIGNAL_PUSH_NOTIFICATION, title, message, url, priority, deviceid, image_url)
