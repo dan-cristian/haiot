41db2198923771ea7cf5d43250ecd914fc0854cf
Dan Cristian <dan.cristian@gmail.com>
2016-09-02 02:50:31 +0300

    change

--- scripts/db/backup-mysql-haiot.sh
+++ scripts/db/backup-mysql-haiot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 DATE=`date +%Y%m%d-%H%M%S`
 OUT_FILE=/mnt/backup/mysql-haiot-backup-$DATE.sql
-mysqldump  --user=haiot --password=haiot --host=localhost --protocol=tcp --port=3306 --default-character-set=utf8 --complete-insert=TRUE --single-transaction=TRUE --routines --events "haiot-reporting" | gzip >  "$OUT_FILE".gz
+OUT_FILE_GZ="$OUT_FILE".gz
+mysqldump  --user=haiot --password=haiot --host=localhost --protocol=tcp --port=3306 --default-character-set=utf8 --complete-insert=TRUE --single-transaction=TRUE --routines --events "haiot-reporting" | gzip >  $OUT_FILE_GZ
+chmod 777 $OUT_FILE_GZ
