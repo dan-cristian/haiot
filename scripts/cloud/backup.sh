a35adebeae7f306da19d375d84c26754fc62d4b2
Dan Cristian <dan.cristian@gmail.com>
2016-09-13 01:06:09 +0300

    fix

--- scripts/cloud/backup.sh
+++ scripts/cloud/backup.sh
@@ -4,7 +4,6 @@ DATE=`date +%Y%m%d-%H%M%S`
 HOST=`cat /etc/hostname`
 BACKUP_DIR=/mnt/backup
 FILE_BACKUP=$BACKUP_DIR/$HOST-Backup$DATE.tar.gz
-FILE_BACKUP_GIT=$BACKUP_DIR/$HOST-GIT-Backup$DATE.tar.gz
 
 echo backing up $FILE_BACKUP
 sleep 5
@@ -12,7 +11,3 @@ sleep 5
 tar -pczf $FILE_BACKUP /  --exclude-from=/home/dcristian/OMV/backup-exclude.txt -v
 chmod 666 $FILE_BACKUP
 
-echo backing up $FILE_BACKUP_GIT
-
-tar -pczf $FILE_BACKUP_GIT /mnt/data/hdd-wdg-6297/git -v
-chmod 666 $FILE_BACKUP_GIT
