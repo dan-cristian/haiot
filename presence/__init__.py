bd6109f9a118a569a241b12e2921474b3c05d9b0
Dan Cristian <dan.cristian@gmail.com>
2016-10-09 13:19:00 +0300

    fix presence

--- presence/__init__.py
+++ presence/__init__.py
@@ -14,23 +14,27 @@ def record_update(json=''):
 
 
 def handle_event_presence(gpio_pin_code='', direction='', pin_value='', pin_connected=None):
-    # Log.logger.info('Presence got event pin {}'.format(gpio_pin_code))
-    zonealarm = models.ZoneAlarm().query_filter_first(
-        models.ZoneAlarm.gpio_host_name.in_([Constant.HOST_NAME]), models.ZoneAlarm.gpio_pin_code.in_([gpio_pin_code]))
-    zone_id = None
-    # fixme: for now zonealarm holds gpio to zone mapping, should be made more generic
-    if zonealarm is not None:
-        zone_id = zonealarm.zone_id
-    if zone_id is not None:
-        current_record = models.Presence().query_filter_first(models.Presence.zone_id == zone_id)
-        record = models.Presence(zone_id=zone_id)
-        record.event_io_date = utils.get_base_location_now_date()
-        record.sensor_name = zonealarm.alarm_pin_name
-        record.save_changed_fields(current_record=current_record, new_record=record, notify_transport_enabled=True,
-                                   save_to_graph=True, save_all_fields=True)
-    else:
-        Log.logger.warning('Unable to find presence zone for pin {}'.format(gpio_pin_code))
-
+    try:
+        # Log.logger.info('Presence got event pin {}'.format(gpio_pin_code))
+        zonealarm = models.ZoneAlarm().query_filter_first(
+            models.ZoneAlarm.gpio_host_name.in_([Constant.HOST_NAME]), models.ZoneAlarm.gpio_pin_code.in_([gpio_pin_code]))
+        zone_id = None
+        # fixme: for now zonealarm holds gpio to zone mapping, should be made more generic
+        if zonealarm is not None:
+            zone_id = zonealarm.zone_id
+        if zone_id is not None:
+            current_record = models.Presence().query_filter_first(models.Presence.zone_id == zone_id)
+            record = models.Presence(zone_id=zone_id)
+            #if current_record is not None:
+            #    record.id = current_record.id
+            record.event_io_date = utils.get_base_location_now_date()
+            record.sensor_name = zonealarm.alarm_pin_name
+            record.save_changed_fields(current_record=current_record, new_record=record, notify_transport_enabled=True,
+                                       save_to_graph=True, save_all_fields=True)
+        else:
+            Log.logger.warning('Unable to find presence zone for pin {}'.format(gpio_pin_code))
+    except Exception, ex:
+        Log.logger.error("Unable to save presence, er={}".format(ex), exc_info=True)
 
 def unload():
     Log.logger.info('Presence module unloading')
