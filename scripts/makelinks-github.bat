140fb8969abbf2b47ab68bb7559c0e90fa82239c
Dan Cristian <dan.cristian@gmail.com>
2016-10-10 00:32:48 +0300

    update

--- scripts/makelinks-github.bat
+++ scripts/makelinks-github.bat
@@ -8,7 +8,9 @@ mklink /J  gpio %T%\gpio
 mklink /J  health_monitor %T%\health_monitor
 mklink /J  heat %T%\heat
 mklink /J  main %T%\main
+mklink /J  music %T%\music
 mklink /J  node %T%\node
+mklink /J  presence %T%\presence
 mklink /J  rule %T%\rule
 mklink /J  scripts %T%\scripts
 mklink /J  sensor %T%\sensor
@@ -16,26 +18,22 @@ mklink /J  sysutils %T%\sysutils
 mklink /J  template %T%\template
 mklink /J  test %T%\test
 mklink /J  transport %T%\transport
+mklink /J  utility %T%\utility
 mklink /J  webui %T%\webui
 
 del LICENSE
 mklink /H LICENSE %T%\LICENSE
 del README.md
 mklink /H README.md %T%\README.md
+
 del haiot.py
 mklink /H haiot.py %T%\haiot.py
-del startserver.sh
-mklink /H startserver.sh %T%\startserver.sh
-del startserver_mem.sh
-mklink /H startserver_mem.sh  %T%\startserver_mem.sh
-del startserver_mem_nosyslog.sh
-mklink /H startserver_mem_nosyslog.sh  %T%\startserver_mem_nosyslog.sh
-del startserver_disk.sh
-mklink /H startserver_disk.sh  %T%\startserver_disk.sh
-del startserver_daemon.sh
-mklink /H startserver_daemon.sh  %T%\startserver_daemon.sh
+
+del start_daemon_userspaces.sh
+mklink /H start_daemon_userspaces.sh  %T%\start_daemon_userspaces.sh
 del startserver.bat
 mklink /H startserver.bat  %T%\startserver.bat
+
 del .gitignore
 mklink /H .gitignore %T%\.gitignore
 del requirements.txt
