551437dae8330c23fbb2cba1f188a8a477e6ec9e
Dan Cristian <dan.cristian@gmail.com>
2016-11-06 13:43:19 +0200

    script improvs

---
+++ scripts/cloud/backup-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DATE=`date +%Y%m%d-%H%M%S`
+HOST=`cat /etc/hostname`
+BACKUP_DIR=/mnt/backup
+FILE_BACKUP_GIT=$BACKUP_DIR/$HOST-GIT-Backup$DATE.tar.gz
+
+echo backing up $FILE_BACKUP_GIT
+
+tar -pczf $FILE_BACKUP_GIT /mnt/data/hdd-wdg-6297/git -v
+chmod 666 $FILE_BACKUP_GIT
