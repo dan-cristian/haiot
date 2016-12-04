d33571bc486e879b1f64c14920cc5a879cee866a
Dan Cristian <dan.cristian@gmail.com>
2016-12-04 22:27:22 +0200

    adds

--- scripts/audio/mpd-init.sh
+++ scripts/audio/mpd-init.sh
@@ -3,16 +3,9 @@
 LOG=/mnt/log/mpd.log
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$DIR/include_cards.sh"
-
-#function echo2(){
-#echo [`date +%T.%N`] $1 $2 $3 $4 $5 >> $LOG 2>&1
-#echo [`date +%T.%N`] $1 $2 $3 $4 $5
-#}
-
+source "$DIR/../common/include_cards.sh"
 
 function init_output() {
-#root_dir=`dirname $0`
 echo2 Initialise all outputs using script in [$DIR]
 $DIR/mpc-play.sh 6600 init
 $DIR/mpc-play.sh 6601 init
