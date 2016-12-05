401d48ccee5c95c9181e46169a7f0c938d66b44a
Dan Cristian <dan.cristian@gmail.com>
2016-12-06 00:44:11 +0200

    dashboard

--- main/admin/model_helper.py
+++ main/admin/model_helper.py
@@ -149,7 +149,7 @@ def populate_tables(model_auto_update=False):
                         models.CommandOverrideRelay, models.PlotlyCache, models.Utility, models.Presence,
                         models.SensorError]
     # tables that will be cleaned on every app start
-    table_force_clean = [models.ZoneHeatRelay, models.Zone, models.Sensor, models.Presence]
+    table_force_clean = [models.ZoneHeatRelay, models.Zone, models.Presence]  #, models.Sensor, ]
 
     for table in table_collection:
         table_str = utils.get_table_name(table)
