ca70a80acbbcd64f3cc364b18568044f9dc0c046
Dan Cristian <dan.cristian@gmail.com>
2017-01-04 00:12:01 +0200

    various

--- scripts/video/i3-mpd.sh
+++ scripts/video/i3-mpd.sh
@@ -2,6 +2,7 @@
 # shell script to prepend i3status with more stuff
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/../common/include_cards.sh"
+source "$DIR/../common/params.sh"
 
 get_card_index_by_name $1
 ind=$?
@@ -10,3 +11,4 @@ mpc -p $port status | grep -q playing
 if [ $? -eq 0 ]; then
 	echo "$1: "$(mpc -p $port current)
 fi
+
