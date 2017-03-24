ad8da4a487ccd7bc737e9d44c94d4b0277846a92
Dan Cristian <dan.cristian@gmail.com>
2017-03-24 23:37:36 +0200

    auto play

--- music/gmusicproxy/__init__.py
+++ music/gmusicproxy/__init__.py
@@ -11,7 +11,7 @@ def get_song_id(artist, title):
     url = get_param(Constant.P_GMUSICPROXY_URL)
     title = urllib.quote(title.encode('utf-8'), safe='')
     artist = urllib.quote(artist.encode('utf-8'), safe='')
-    param = '/search_id?type=song&title={}&artist={}&exact=yes'.format(title, artist)
+    param = '/search_id?type=song&title={}&artist={}&exact=no'.format(title, artist)
     result = requests.get('{}{}'.format(url, param)).text
     if result == '':
         return None
