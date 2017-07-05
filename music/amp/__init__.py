5be200be50cfc552e8ecf4b98d57e76b2e450dd0
Dan Cristian <dan.cristian@gmail.com>
2017-07-06 01:25:57 +0300

    debug socket

--- music/amp/__init__.py
+++ music/amp/__init__.py
@@ -66,6 +66,7 @@ def _amp_bi_set_yamaha(on, sock):
 
 
 def amp_zone_power(on, zone_index):
+    Log.logger.info("Setting amp power for zone {}".format(zone_index))
     global _AMP_ZONE3_POWER_OFF, _AMP_ZONE3_POWER_ON
     sock = connect_socket()
     msg = "socket cmd ok, "
@@ -98,44 +99,33 @@ def amp_zone_power(on, zone_index):
 
 
 def set_amp_power(power_state, relay_name, amp_zone_index):
-    Log.logger.info("step 1")
     relay = models.ZoneCustomRelay.query.filter_by(relay_pin_name=relay_name).first()
-    Log.logger.info("step 2")
     power_state = bool(power_state)
-    Log.logger.info("step 2.1")
     if relay is not None:
         initial_relay_state = relay.relay_is_on
         # power on main relay for amp or on/off if there is no zone
-        Log.logger.info("step 2.2")
         if power_state is True or amp_zone_index == 0:
-            Log.logger.info("step 3")
             relay.relay_is_on = power_state
             commit()
-            Log.logger.info("step 4")
             # dispatch as UI action otherwise change actions are not triggered
             dispatcher.send(signal=Constant.SIGNAL_UI_DB_POST, model=models.ZoneCustomRelay, row=relay)
             msg = "Set relay {} to state {} zone_index={}\n".format(relay_name, power_state, amp_zone_index)
             Log.logger.info(msg)
         else:
-            Log.logger.info("step 5")
             msg = "Not changed relay state for {}\n".format(relay_name)
     else:
         msg = "Could not find relay name {}\n".format(relay_name)
         Log.logger.warning(msg)
         return msg
 
-    Log.logger.info("step 5")
     # change amp zone power
     if amp_zone_index is None or amp_zone_index == 0:
         # only main relay change is needed
-        Log.logger.info("step 6")
         return msg + "Power in {} set to {}\n".format(relay_name, relay.relay_is_on)
     else:
-        Log.logger.info("step 7")
         # potentially amp settings change is required to switch amp zones
         if initial_relay_state is not True and power_state is True:
             # delay to wait for amp to fully start
-            Log.logger.info("step 8")
             time.sleep(5)
-        Log.logger.info("step 9")
-        return msg + amp_zone_power(power_state, amp_zone_index)
+        result_amp = amp_zone_power(power_state, amp_zone_index)
+        return msg + result_amp
