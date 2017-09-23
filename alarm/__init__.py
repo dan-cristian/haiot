42a95ddb98df6bb21a6eb98bfd882224f7286468
Dan Cristian <dan.cristian@gmail.com>
2017-09-23 18:10:56 +0300

    fix alarm init

--- alarm/__init__.py
+++ alarm/__init__.py
@@ -52,7 +52,7 @@ def init():
     for alarm in local_alarms:
         gpio_pin = models.GpioPin().query_filter_first(models.GpioPin.pin_code.in_([alarm.gpio_pin_code]),
                                                        models.GpioPin.host_name.in_([Constant.HOST_NAME]))
-        if gpio_pin:
+        if gpio_pin is not None:
             # Log.logger.info('Schedule setup alarm port pin={} type={}'.format(gpio_pin.pin_code, gpio_pin.pin_type))
             port_list.append(gpio_pin)
         else:
