d7cb8e2b2e66c66acb2d8bfbc71b79af055d5a1e
Dan Cristian <dan.cristian@gmail.com>
2017-10-10 09:17:32 +0300

    ups debug & save debug

--- main/admin/event.py
+++ main/admin/event.py
@@ -170,8 +170,9 @@ def mqtt_thread_run():
                         if obj[Constant.JSON_PUBLISH_SAVE_TO_HISTORY] and Constant.HAS_LOCAL_DB_REPORTING_CAPABILITY:
                             persistence.save_to_history_db(obj)
                         else:
-                            Log.logger.info("Not saving to db, json publish={} has db={}".format(
-                                obj[Constant.JSON_PUBLISH_SAVE_TO_HISTORY], Constant.HAS_LOCAL_DB_REPORTING_CAPABILITY))
+                            Log.logger.info("Not saving to db, json publish={} has db={}, obj={}".format(
+                                obj[Constant.JSON_PUBLISH_SAVE_TO_HISTORY], Constant.HAS_LOCAL_DB_REPORTING_CAPABILITY,
+                                obj))
                         # if record is marked to be uploaded to a graph
                         if Constant.JSON_PUBLISH_SAVE_TO_GRAPH in obj and obj[Constant.JSON_PUBLISH_SAVE_TO_GRAPH]:
                             pass
