0e1d958eb8473b785377947f07a908d243e554f7
Dan Cristian <dan.cristian@gmail.com>
2017-10-23 00:02:38 +0300

    ups

--- cloud/email/__init__.py
+++ cloud/email/__init__.py
@@ -9,6 +9,9 @@ from pydispatch import dispatcher
 __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
 initialised = False
+__notif_from = None
+__notif_pass = None
+__notif_dest = None
 
 
 def _get_pass(email=None):
@@ -25,10 +28,11 @@ def _get_pass(email=None):
 
 
 def send_notification(subject=None, body=None):
-    sent_from = model_helper.get_param(Constant.P_GMAIL_NOTIFY_FROM_EMAIL)
-    notify_recipient = model_helper.get_param(Constant.P_NOTIFY_EMAIL_RECIPIENT)
-    gmail_password = _get_pass(sent_from)
-    if gmail_password is not None:
+    global __notif_from, __notif_pass, __notif_dest
+    sent_from = __notif_from
+    notify_recipient = __notif_dest
+
+    if __notif_pass is not None:
         to = [notify_recipient]
         email_text = "\r\n".join([
             "From: %s" % sent_from,
@@ -40,7 +44,7 @@ def send_notification(subject=None, body=None):
         try:
             server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             server.ehlo()
-            server.login(sent_from, gmail_password)
+            server.login(sent_from, __notif_pass)
             server.sendmail(sent_from, to, email_text)
             server.close()
             return True
@@ -61,6 +65,11 @@ def unload():
 def init():
     Log.logger.info('Email module initialising')
     dispatcher.connect(send_notification, signal=Constant.SIGNAL_EMAIL_NOTIFICATION, sender=dispatcher.Any)
+    global __notif_from, __notif_pass, __notif_dest
+    __notif_from = model_helper.get_param(Constant.P_GMAIL_NOTIFY_FROM_EMAIL)
+    __notif_dest = model_helper.get_param(Constant.P_NOTIFY_EMAIL_RECIPIENT)
+    __notif_pass = _get_pass(__notif_from)
+
     if send_notification("Starting haiot", "Host is {}".format(Constant.HOST_NAME)):
         Log.logger.info("Init email sent OK")
     global initialised
