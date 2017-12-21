e8600eaab70ac44b74b2afeefb3f633b27e9ba11
Dan Cristian <dan.cristian@gmail.com>
2017-12-22 00:34:45 +0200

    dashcam accel

--- apps/dashcam/accel/__init__.py
+++ apps/dashcam/accel/__init__.py
@@ -5,6 +5,7 @@ import web
 import time
 import threading
 try:
+    import smbus
     from mpu6050 import mpu6050
 except Exception, ex:
     print ex
@@ -14,13 +15,59 @@ __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
 class Params:
     lastrecord = None
-    sensor = None
+    #sensor = None
     calibration = None
     sensitivity = 1 # number of decimals
     WEB_PORT = 9000 # set to 0 to disable
     urls = ('/', 'Index')
     web_thread = None
     web_app = None
+
+
+
+class Raw:
+    # Power management registers
+    power_mgmt_1 = 0x6b
+    power_mgmt_2 = 0x6c
+
+    gyro_scale = 131.0
+    accel_scale = 16384.0
+
+    address = 0x68  # This is the default I2C address of ITG-MPU breakout board
+    bus = None
+
+    @staticmethod
+    def init():
+        Raw.bus = smbus.SMBus(1)
+        # Now wake the 6050 up as it starts in sleep mode
+        Raw.bus.write_byte_data(Raw.address, Raw.power_mgmt_1, 0)
+
+    @staticmethod
+    def twos_compliment(val):
+        if val >= 0x8000:
+            return -((65535 - val) + 1)
+        else:
+            return val
+
+    @staticmethod
+    def read_all():
+        raw_gyro_data = Raw.bus.read_i2c_block_data(Raw.address, 0x43, 6)
+        raw_accel_data = Raw.bus.read_i2c_block_data(Raw.address, 0x3b, 6)
+        raw_temp_data = Raw.bus.read_i2c_block_data(Raw.address, 0x41, 6)
+        temp = (raw_temp_data / 340.0) + 36.53
+
+        gyro_scaled_x = Raw.twos_compliment((raw_gyro_data[0] << 8) + raw_gyro_data[1]) / Raw.gyro_scale
+        gyro_scaled_y = Raw.twos_compliment((raw_gyro_data[2] << 8) + raw_gyro_data[3]) / Raw.gyro_scale
+        gyro_scaled_z = Raw.twos_compliment((raw_gyro_data[4] << 8) + raw_gyro_data[5]) / Raw.gyro_scale
+
+        accel_scaled_x = Raw.twos_compliment((raw_accel_data[0] << 8) + raw_accel_data[1]) / Raw.accel_scale
+        accel_scaled_y = Raw.twos_compliment((raw_accel_data[2] << 8) + raw_accel_data[3]) / Raw.accel_scale
+        accel_scaled_z = Raw.twos_compliment((raw_accel_data[4] << 8) + raw_accel_data[5]) / Raw.accel_scale
+
+        return (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z, temp)
+
+
+############
 
 
 def _represent():
@@ -48,8 +95,8 @@ class Index:
 
 
 def calibrate():
-    Params.calibration = Params.sensor.get_all_data()
-
+    #Params.calibration = Params.sensor.get_all_data()
+    pass
 
 def _run_web_server():
     try:
@@ -61,7 +108,8 @@ def _run_web_server():
 
 def init():
     try:
-        Params.sensor = mpu6050(0x68)
+        #Params.sensor = mpu6050(0x68)
+        Raw.init()
     except Exception, ex:
         print ex
 
@@ -73,10 +121,16 @@ def init():
 
 def read_sensor():
     try:
-        sensor_all = Params.sensor.get_all_data()
+        #sensor_all = Params.sensor.get_all_data()
+        (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z,
+         accel_scaled_x, accel_scaled_y, accel_scaled_z, temp) = Raw.read_all()
+
+        sensor_all = [{'y': accel_scaled_y, 'x': accel_scaled_x, 'z': accel_scaled_z},
+                      {'y': gyro_scaled_y, 'x': gyro_scaled_x, 'z': gyro_scaled_z}, temp]
     except Exception, ex:
         print ex
-        sensor_all = [{'y': -0.49320554199218747, 'x': -9.528922607421874, 'z': 1.0414777221679687}, {'y': -0.8625954198473282, 'x': -3.0839694656488548, 'z': 0.7938931297709924}, 28.577058823529413]
+        sensor_all = [{'y': -0.49320554199218747, 'x': -9.528922607421874, 'z': 1.0414777221679687},
+                      {'y': -0.8625954198473282, 'x': -3.0839694656488548, 'z': 0.7938931297709924}, 99]
     Params.lastrecord = sensor_all
     return sensor_all
 
@@ -91,7 +145,7 @@ if __name__ == "__main__":
     try:
         while True:
             thread_run()
-            time.sleep(0.2)
+            time.sleep(0.1)
     finally:
         Params.web_app.stop()
         Params.web_thread.join()
