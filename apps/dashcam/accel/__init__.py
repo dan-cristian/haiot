043b6478bd3ff2fea1831e15e16a083bf3676966
Dan Cristian <dan.cristian@gmail.com>
2017-12-21 23:13:32 +0200

    dashcam accel

--- apps/dashcam/accel/__init__.py
+++ apps/dashcam/accel/__init__.py
@@ -11,54 +11,49 @@ except Exception, ex:
 
 __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
-_sensor = None
-_lastrecord = None
-_calibration = None
-_sensitivity = 1 # number of decimals
+
+class Params:
+    lastrecord = None
+    sensor = None
+    calibration = None
+    sensitivity = 1 # number of decimals
+    WEB_PORT = 9000 # set to 0 to disable
+    urls = ('/', 'Index')
+    web_thread = None
+    web_app = None
 
 
-_WEB_PORT=9000 # set to 0 to disable
-_urls = ('/', 'index')
-_web_thread = None
-_web_app = None
+def _represent():
+    last = Params.lastrecord
+    res = None
+    try:
+        res = "[{'y': %f, 'x': %f, 'z': %f}, {'y': %f, 'x': %f, 'z': %f}, {%f}]" % \
+              (last[0]['y'], last[0]['x'], last[0]['z'],
+               last[1]['y'], last[1]['x'], last[1]['z'],
+               last[2])
+    except Exception, ex:
+        print ex
+    return res
 
 
-class AccelRecord:
-    def __init__(self, accel, gyro, temp):
-        self.accel, self.gyro, self.temp = accel, gyro, temp
-
-    def __repr__(self):
-        try:
-            res = "[{'y': %f, 'x': %f, 'z': %f}, {'y': %f, 'x': %f, 'z': %f}, {%f}]" % \
-                  (self.accel['y'] , self.accel['x'], self.accel['z'], self.gyro['y'], self.gyro['x'], self.gyro['z'],
-                   self.temp)
-        except Exception, ex:
-            pass
-        return res
+def _to_json():
+    return _represent()
 
 
-    def to_json(self):
-        return json.dumps(self)
-
-
-class index:
+class Index:
     def GET(self):
-        record = _lastrecord
-        print "returning {}".format(record)
-        return record
+        print "returning {}".format(_represent())
+        return _represent()
 
 
 def calibrate():
-    global _calibration
-    sensor_all = _sensor.get_all_data()
-    _calibration = sensor_all
+    Params.calibration = Params.sensor.get_all_data()
 
 
 def _run_web_server():
-    global _web_app
     try:
-        _web_app = web.application(_urls, globals())
-        web.httpserver.runsimple(_web_app.wsgifunc(), ("0.0.0.0", _WEB_PORT))
+        Params.web_app = web.application(Params.urls, globals())
+        web.httpserver.runsimple(Params.web_app.wsgifunc(), ("0.0.0.0", Params.WEB_PORT))
     except Exception, ex:
         print ex
 
@@ -70,21 +65,21 @@ def init():
     except Exception, ex:
         print ex
 
-    if _WEB_PORT != 0:
-        _web_thread = threading.Thread(target=_run_web_server)
-        _web_thread.start()
+    if Params.WEB_PORT != 0:
+        Params.web_thread = threading.Thread(target=_run_web_server)
+        Params.web_thread.name = 'WebAccel'
+        Params.web_thread.start()
 
 
 def read_sensor():
-    global _lastrecord
     try:
-        sensor_all = _sensor.get_all_data()
+        sensor_all = Params.sensor.get_all_data()
     except Exception, ex:
-        print ex
+        #print ex
         sensor_all = [{'y': -0.49320554199218747, 'x': -9.528922607421874, 'z': 1.0414777221679687}, {'y': -0.8625954198473282, 'x': -3.0839694656488548, 'z': 0.7938931297709924}, 28.577058823529413]
-    record = AccelRecord(sensor_all[0], sensor_all[1], sensor_all[2])
-    _lastrecord = record
-    return record
+    Params.lastrecord = sensor_all
+    return sensor_all
+
 
 def thread_run():
     read_sensor()
@@ -98,5 +93,5 @@ if __name__ == "__main__":
             thread_run()
             time.sleep(0.2)
     finally:
-        _web_app.stop()
-        _web_thread.join()
+        Params.web_app.stop()
+        Params.web_thread.join()
