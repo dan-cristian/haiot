bdaefa2c169b647078ddb6904246f3d5595ff4e3
Dan Cristian <dan.cristian@gmail.com>
2017-12-23 12:55:46 +0200

    internet

--- scripts/common/haveinternet.sh
+++ scripts/common/haveinternet.sh
@@ -51,7 +51,8 @@ function pingdns
   fi
 }
 
-function httpreq{
+function httpreq
+{
   tput setaf 6; echo && echo "Checking for HTTP Connectivity"; tput sgr0;
   case "$(curl -s --max-time 2 -I $checkdomain | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
     [23]) tput setaf 2; echo "HTTP connectivity is up"; tput sgr0;;
@@ -61,22 +62,22 @@ function httpreq{
 #  exit 0
 }
 
-function checkgw {
-#Ping gateway first to verify connectivity with LAN
-tput setaf 6; echo "Pinging gateway ($GW) to check for LAN connectivity" && echo; tput sgr0;
-if [ "$GW" = "" ]; then
-    tput setaf 1;echo "There is no gateway. Probably disconnected..."; tput sgr0;
-#    exit 1
-fi
-ping $GW -c $pcount
-return $?
+function checkgw
+{
+    #Ping gateway first to verify connectivity with LAN
+    tput setaf 6; echo "Pinging gateway ($GW) to check for LAN connectivity" && echo; tput sgr0;
+    if [ "$GW" = "" ]; then
+        tput setaf 1;echo "There is no gateway. Probably disconnected..."; tput sgr0;
+    #    exit 1
+    fi
+    ping $GW -c $pcount
+    return $?
 }
 
 tput setaf 6; echo "Fast check for HTTPS connectivity" && echo; tput sgr0;
 if portscan; then exit 0; fi
 
 checkgw
-
 if [ $? -eq 0 ]
 then
   tput setaf 6; echo && echo "LAN Gateway pingable. Proceeding with internet connectivity check."; tput sgr0;
@@ -91,7 +92,5 @@ else
   pingnet
   portscan
   httpreq
-
-  #Insert any command you like here
-#  exit 1
+  #  exit 1
 fi
