6d9025ef9f115de6f9b93b0511564882681ce578
Dan Cristian <dan.cristian@gmail.com>
2017-12-25 12:21:34 +0200

    dash

---
+++ apps/dashcam/recorder/usb_tool.py
@@ -0,0 +1,29 @@
+import subprocess
+import shlex
+__author__ = 'Dan Cristian<dan.cristian@gmail.com>'
+
+
+def _get_usb_dev_root(dev_name):
+    cmd = 'tail /sys/devices/platform/soc/*/*/*/*/product'
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
+    prev_line = None
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            if dev_name in output:
+                break
+            else:
+                prev_line = output
+    rc = process.poll()
+    if prev_line is not None:
+        prev_line = prev_line.replace('==> ', '').replace(' <==', '')
+    return prev_line
+
+
+if __name__ == '__main__':
+    print _get_usb_dev_root('C525')
+
+
+
