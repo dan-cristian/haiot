b9033db9f38024495f094b7d1a04bbebc9497476
Dan Cristian <dan.cristian@gmail.com>
2017-12-25 14:25:20 +0200

    dash

--- apps/dashcam/recorder/usb_tool.py
+++ apps/dashcam/recorder/usb_tool.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+from usb.core import find as finddev
+
 #import shlex
 __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
@@ -25,6 +27,21 @@ def _get_usb_dev_root(dev_name):
         prev_line = prev_line.replace('==> ', '').replace(' <==', '').replace('product', '').strip()
         # /sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.5/
     return prev_line
+
+
+def _get_usb_vendor_prod(dev_name):
+    rec = subprocess.check_output(['lsusb']).split('\n')
+    vendor = product = None
+    for line in rec:
+        if dev_name in line:
+            atoms = line.split(' ID ')
+            if len(atoms) > 1:
+                ven_prod = atoms.split(' ')[0].split(':')
+                vendor = ven_prod[0]
+                product = ven_prod[1]
+                print "Vendor:Product for {} is {}:{}".format(dev_name, vendor, product)
+                break
+    return vendor, product
 
 
 def get_usb_dev(dev_name):
@@ -55,8 +72,20 @@ def get_usb_audio(dev_name):
     return res
 
 
+def recover_usb_video(dev_name):
+    # rmmod uvcvideo
+    # modprobe uvcvideo
+    vendor, product = _get_usb_vendor_prod(dev_name)
+    dev = finddev(idVendor=hex(vendor), idProduct=hex(product))
+    if dev is not None:
+        print "Reseting USB {}".format(dev)
+        dev.reset()
+
+
 if __name__ == '__main__':
     print _get_usb_dev_root('C525')
     print get_usb_dev('C525')
+    print _get_usb_vendor_prod('C525')
+    recover_usb_video('C525')
 
 
