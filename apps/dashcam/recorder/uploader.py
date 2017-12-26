6a21fc1761ccc736aac82a87a59519b167de7582
Dan Cristian <dan.cristian@gmail.com>
2017-12-26 02:45:05 +0200

    dash

--- apps/dashcam/recorder/uploader.py
+++ apps/dashcam/recorder/uploader.py
@@ -2,7 +2,10 @@ import subprocess
 from stat import S_ISREG, ST_CTIME, ST_MODE, ST_MTIME
 import os, sys, time, datetime
 import shutil
+from collections import namedtuple
 
+disk_ntuple = namedtuple('partition',  'device mountpoint fstype')
+usage_ntuple = namedtuple('usage',  'total used free percent')
 
 class P():
     server = 'haiot@192.168.0.18'
@@ -12,7 +15,55 @@ class P():
     root_folder = None
     move_folder = None
     exclude_time_delta = 120 # exclude files modified in the last x seconds
+    days_to_keep = 30
+    max_disk_used_percent = 80 #
     folder_dict = set()
+
+
+def disk_partitions(all_part=False):
+    """Return all mountd partitions as a nameduple.
+    If all == False return phyisical partitions only.
+    """
+    #https://stackoverflow.com/questions/4260116/find-size-and-free-space-of-the-filesystem-containing-a-given-file
+    phydevs = []
+    f = open("/proc/filesystems", "r")
+    for line in f:
+        if not line.startswith("nodev"):
+            phydevs.append(line.strip())
+
+    retlist = []
+    f = open('/etc/mtab', "r")
+    for line in f:
+        if not all_part and line.startswith('none'):
+            continue
+        fields = line.split()
+        device = fields[0]
+        mountpoint = fields[1]
+        fstype = fields[2]
+        if not all_part and fstype not in phydevs:
+            continue
+        if device == 'none':
+            device = ''
+        ntuple = disk_ntuple(device, mountpoint, fstype)
+        retlist.append(ntuple)
+    return retlist
+
+
+def disk_usage(path):
+    """Return disk usage associated with path."""
+    st = os.statvfs(path)
+    free = (st.f_bavail * st.f_frsize)
+    total = (st.f_blocks * st.f_frsize)
+    used = (st.f_blocks - st.f_bfree) * st.f_frsize
+    try:
+        percent = ret = (float(used) / total) * 100
+    except ZeroDivisionError:
+        percent = 0
+    # NB: the percentage is -5% than what shown by df due to
+    # reserved blocks that we are currently not considering:
+    # http://goo.gl/sWGbH
+    return usage_ntuple(total, used, free, round(percent, 1))
+
 
 
 def _upload_file(file_path, file_date):
@@ -31,7 +82,7 @@ def _upload_file(file_path, file_date):
 
 
 
-def _file_list(folder, exclude_delta):
+def _file_list(folder, exclude_delta=0):
     # https://stackoverflow.com/questions/168409/how-do-you-get-a-directory-listing-sorted-by-creation-date-in-python
     # get all entries in the directory w/ stats
     entries = (os.path.join(folder, fn) for fn in os.listdir(folder))
@@ -65,11 +116,28 @@ def _upload():
             print('Exception uploading file {}, ex={}'.format(file[0], ex))
 
 
+def _clean():
+    files = _file_list(P.move_folder)
+    for file in files:
+        now = time.time()
+        try:
+            delta_days = (now - file[1]) / (60*60*24)
+            if delta_days > P.days_to_keep:
+                os.remove(file[0])
+                print('Old {} days file deleted {}'.format(delta_days, file[0]))
+        except Exception, ex:
+            pass
+
+
 def thread_run():
-    _upload
+    _upload()
+    _clean()
 
 
 if __name__ == '__main__':
+    for part in disk_partitions():
+        print(part)
+        print("    %s\n" % str(disk_usage(part.mountpoint)))
     P.root_folder = '/home/haiot/recordings/'
     P.move_folder = '/home/haiot/recordings/uploaded/'
-    _upload()
+    #_upload()
