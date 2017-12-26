eb7cd9f293b4004209a126cfddefc78675588081
Dan Cristian <dan.cristian@gmail.com>
2017-12-26 21:39:18 +0200

    dash

--- apps/dashcam/recorder/utils.py
+++ apps/dashcam/recorder/utils.py
@@ -4,7 +4,7 @@ import subprocess
 # 4222 pts/1    Ss     0:00 -bash
 # 4555 pts/1    R      8:49 ffmpeg -y -f alsa -thread_queue_size 8192 -ac 1 -i hw:1,0 -r 8 -f video4linux2 -thread_queue_size 8192 -i /dev/v4l/by-id/usb-046d_HD_
 def get_proc(keywords):
-    out = subprocess.check_output(['ps', 'aw']).split('\n')
+    out = subprocess.check_output(['ps', 'aww']).split('\n')
     res = None
     for line in out:
         if keywords in line:
