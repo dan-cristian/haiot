68dfda236bb33886dc88e04a366310a0e5fb5747
Dan Cristian <dan.cristian@gmail.com>
2017-12-27 18:18:33 +0200

    dash

--- apps/dashcam/recorder/utils.py
+++ apps/dashcam/recorder/utils.py
@@ -4,13 +4,16 @@ import subprocess
 # 4222 pts/1    Ss     0:00 -bash
 # 4555 pts/1    R      8:49 ffmpeg -y -f alsa -thread_queue_size 8192 -ac 1 -i hw:1,0 -r 8 -f video4linux2 -thread_queue_size 8192 -i /dev/v4l/by-id/usb-046d_HD_
 def get_proc(keywords):
-    out = subprocess.check_output(['ps', 'aww']).split('\n')
     res = None
-    for line in out:
-        if keywords in line:
-            atoms = line.strip().split(' ')
-            if atoms[0].isdigit():
-                res = int(atoms[0])
+    try:
+        out = subprocess.check_output(['ps', 'aww']).split('\n')
+        for line in out:
+            if keywords in line:
+                atoms = line.strip().split(' ')
+                if atoms[0].isdigit():
+                    res = int(atoms[0])
+    except Exception, ex:
+        print("Got exception on proc find, err={}".format(ex))
     return res
 
 
