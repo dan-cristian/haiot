4d43ffd911daca65d999b35533fd316a97845872
Dan Cristian <dan.cristian@gmail.com>
2018-01-03 11:51:01 +0200

    refactor things

--- cloud/thingspeak/__init__.py
+++ cloud/thingspeak/__init__.py
@@ -5,18 +5,20 @@ from main.logger_helper import L
 from pydispatch import dispatcher
 import thingspeak
 import datetime
+import traceback
 initialised = False
 
 
 class P:
-    upload_prefix = "upload_"
+    key_separator = ':'
+    fields = {}
+    keys = {}
     channels = {}
-    upload = {}
     last_upload_ok = datetime.datetime.now()
 
 
 def _upload_field(model, fields):
-    upload = P.channels[P.upload_prefix + model]
+    upload = P.channels[model]
     res = upload.update(fields)
     if res == 0:
         delta = (datetime.datetime.now() - P.last_upload_ok).total_seconds()
@@ -40,23 +42,30 @@ def _handle_record(record=None):
             L.l.warning("Something is wrong in class name, does not contain expected termination")
     try:
         if model is not None:
-            if model in P.channels:
-                cloud_model = P.channels[model][0]  # only one instance supported for now
+            if model in P.fields:
+                cloud_fields = P.fields[model]  # only one instance supported for now
+                #cloud_keys = P.keys[model]
                 field_index = 1
                 fields = {}
-                for cloud_field in cloud_model:
-                    if hasattr(record, cloud_field):
-                        if hasattr(record, Constant.DB_FIELD_UPDATE):
-                            created_at = getattr(record, Constant.DB_FIELD_UPDATE)
-                        else:
-                            created_at = None
-                        fields['field' + str(field_index)] =  getattr(record, cloud_field)
-                        #_upload_field(model, field_index, getattr(record, cloud_field), created_at)
+                for cloud_field in cloud_fields:
+                    cloud_field_name = cloud_field[0]
+                    if hasattr(record, cloud_field_name):
+                        cloud_key_val = cloud_field[1]
+                        record_key_val = getattr(record, cloud_field[2])
+                        if record_key_val == cloud_key_val:
+                            fields['field' + str(field_index)] = getattr(record, cloud_field_name)
+                            #_upload_field(model, field_index, getattr(record, cloud_field), created_at)
                     field_index += 1
-                fields['created_at'] = created_at
+                if len(fields) > 0:
+                    if hasattr(record, Constant.DB_FIELD_UPDATE):
+                        created_at = getattr(record, Constant.DB_FIELD_UPDATE)
+                    else:
+                        created_at = None
+                    fields['created_at'] = created_at
                 _upload_field(model, fields)
     except Exception, ex:
         L.l.error("Unable to upload record, ex={}".format(ex))
+
 
 def init():
     global initialised
@@ -64,35 +73,61 @@ def init():
         config_file = model_helper.get_param(Constant.P_THINGSPEAK_API_FILE)
         with open(config_file, 'r') as f:
             channel_list = json.load(f)
-            for id in channel_list:
-                read_api = channel_list[id]['read_api']
-                write_api = channel_list[id]['write_api']
-                ch = thingspeak.Channel(id, api_key=read_api)
+            for chid in channel_list:
+                read_api = channel_list[chid]['read_api']
+                write_api = channel_list[chid]['write_api']
+                ch = thingspeak.Channel(chid, api_key=read_api)
                 ch.write_key = write_api
-                params = ch.get({'results': 0, 'metadata': 'true'})
+                for i in range(1, 2):
+                    try:
+                        params = ch.get({'results': 0, 'metadata': 'true'})
+                        break
+                    except Exception, ex:
+                        L.l.warning("Error while getting cloud data, ex={}".format(ex))
                 setup = json.loads(params)['channel']
-                atoms = setup['metadata'].split('=')
-                if atoms[0] == 'model':
-                    model = atoms[1]
-                    # setup['model'] = model
-                    fields = []
+                meta = setup['metadata']
+                if ',' not in meta:
+                    meta = meta + ','
+                atoms = meta.split(',')
+                model = None
+                key = None
+                fields = []
+                for atom in atoms:
+                    pair = atom.split('=')
+                    if len(pair) == 2:
+                        kname = pair[0]
+                        kval = pair[1]
+                        if kname == 'model':
+                            model = kval
+                        elif kname == "key":
+                            key = kval
+                    else:
+                        L.l.warning("Wrong meta format, <key=value> expected, got instead {}".format(atom))
+                if model is None:
+                    L.l.warning("Expected model=TableName not found in metadata {}".format(setup['metadata']))
+                else:
+                    P.channels[model] = ch
+                    if key is not None:
+                        P.keys[model] = key
+                        # setup['model'] = model
                     for f in range(1, 8):
                         field = 'field' + str(f)
+                        key_val = None
                         if field in setup:
-                            fields.append(setup[field])
+                            fname = setup[field]
+                            if key is not None and P.key_separator in fname:
+                                fpair = fname.split(P.key_separator)
+                                fname = fpair[0]
+                                key_val = fpair[1]
+                            fields.append([fname, key_val, key])
                         else:
                             break
-                    if model in P.channels:
-                        #fixme: add support for
-                        #P.channels[model].append(fields)
+                    if model in P.fields:
+                        # fixme: add support for
+                        # P.channels[model].append(fields)
                         L.l.error("Multiple channels with same field not yet supported")
-                    else:
-                        P.channels[model] = [fields]
-                        P.channels[P.upload_prefix + model] = ch
-                else:
-                    model = None
-                    L.l.warning("Expected model=TableName not found in metadata {}".format(setup['metadata']))
+                    P.fields[model] = fields
         initialised = True
         dispatcher.connect(_handle_record, signal=Constant.SIGNAL_STORABLE_RECORD, sender=dispatcher.Any)
     except Exception, ex:
-        L.l.warning("Unable to read config or init thingspeak")
+        L.l.warning("Unable to read config or init thingspeak, stack={}".format(traceback.print_exc()))
