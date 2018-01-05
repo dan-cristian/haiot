f5f22e8a556088b76ec2119edac155f81a5db222
Dan Cristian <dan.cristian@gmail.com>
2018-01-05 23:43:25 +0200

    parallel

--- main/admin/models.py
+++ main/admin/models.py
@@ -499,7 +499,6 @@ class ZoneSensor(db.Model, DbBase):
     zone_id = db.Column(db.Integer, db.ForeignKey('zone.id'))
     # zone = db.relationship('Zone', backref=db.backref('ZoneSensor(zone)', lazy='dynamic'))
     sensor_address = db.Column(db.String(50), db.ForeignKey('sensor.address'))
-
     # sensor = db.relationship('Sensor', backref=db.backref('ZoneSensor(sensor)', lazy='dynamic'))
 
     def __init__(self, zone_id='', sensor_address='', sensor_name=''):
