47a23bbb51579dc3b6eb96e86c371afc7bbc123c
Dan Cristian <dan.cristian@gmail.com>
2018-01-20 15:57:48 +0200

    INA power sensor

--- apps/dashcam/__init__.py
+++ apps/dashcam/__init__.py
@@ -1,6 +1,8 @@
 from main.logger_helper import L
 from main import thread_pool
+from common import Constant
 import ui
+from pydispatch import dispatcher
 #import rpusbdisp
 import recorder
 from recorder import uploader
@@ -9,6 +11,15 @@ import accel
 __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
 initialised = False
+
+
+class P:
+    battery_max_voltage = 4.1
+    #battery_min_voltage = 3.5
+    battery_warn_voltage = 3.6
+    battery_sleep_voltage = 3.4
+    battery_shutdown_voltage = 3.4
+    battery_warn_current = 1000
 
 
 def unload():
@@ -24,18 +35,34 @@ def unload():
     initialised = False
 
 
+def _battery_stat(battery, voltage, current, power):
+    if voltage <= P.battery_warn_voltage:
+        L.l.warning("Battery voltage is very low at {}".format(voltage))
+        pass
+    if voltage <= P.battery_shutdown_voltage:
+        L.l.warning("Battery voltage is too low at {}, shutting down system".format(voltage))
+        pass
+    L.l.info("Battery {} v={} c={} p={}".format(battery, voltage, current, power))
+
+
+def thread_run():
+    uploader.thread_run()
+    gps.thread_run()
+
+
 def init():
     L.l.info('Dashcam module initialising')
     recorder.init()
     thread_pool.add_interval_callable(recorder.thread_run, run_interval_second=recorder.thread_tick)
-    thread_pool.add_interval_callable(uploader.thread_run, run_interval_second=10)
+    thread_pool.add_interval_callable(thread_run, run_interval_second=10)
     gps.init()
-    thread_pool.add_interval_callable(gps.thread_run, run_interval_second=10)
+    dispatcher.connect(_battery_stat, signal=Constant.SIGNAL_BATTERY_STAT, sender=dispatcher.Any)
     #accel.init()
     #thread_pool.add_interval_callable(accel.thread_run, run_interval_second=0.5)
     #ui.init()
     global initialised
     initialised = True
+
 
 if __name__ == '__main__':
     pass
