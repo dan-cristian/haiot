dd99686b492a5726d8322ccc22f638ec766c5603
Dan Cristian <dan.cristian@gmail.com>
2018-01-21 23:54:09 +0200

    accel

--- apps/dashcam/accel/__init__.py
+++ apps/dashcam/accel/__init__.py
@@ -4,8 +4,8 @@ import json
 import web
 import time
 import threading
+from main.logger_helper import L
 try:
-    #import smbus
     from smbus2 import SMBus
     #from mpu6050 import mpu6050
 except Exception, ex:
@@ -16,7 +16,6 @@ __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
 class P:
     lastrecord = None
-    #sensor = None
     calibration = None
     WEB_PORT = 9000 # set to 0 to disable
     urls = ('/', 'Index')
@@ -29,10 +28,8 @@ class Raw:
     # Power management registers
     power_mgmt_1 = 0x6b
     power_mgmt_2 = 0x6c
-
     gyro_scale = 131.0
     accel_scale = 16384.0
-
     address = 0x68  # This is the default I2C address of ITG-MPU breakout board
     bus = None
 
@@ -55,15 +52,12 @@ class Raw:
         raw_accel_data = Raw.bus.read_i2c_block_data(Raw.address, 0x3b, 6)
         raw_temp_data = Raw.bus.read_i2c_block_data(Raw.address, 0x41, 6)
         temp = (Raw.twos_compliment((raw_temp_data[0] << 8) + raw_temp_data[1]) / 340.0) + 36.53
-
         gyro_scaled_x = Raw.twos_compliment((raw_gyro_data[0] << 8) + raw_gyro_data[1]) / Raw.gyro_scale
         gyro_scaled_y = Raw.twos_compliment((raw_gyro_data[2] << 8) + raw_gyro_data[3]) / Raw.gyro_scale
         gyro_scaled_z = Raw.twos_compliment((raw_gyro_data[4] << 8) + raw_gyro_data[5]) / Raw.gyro_scale
-
         accel_scaled_x = Raw.twos_compliment((raw_accel_data[0] << 8) + raw_accel_data[1]) / Raw.accel_scale
         accel_scaled_y = Raw.twos_compliment((raw_accel_data[2] << 8) + raw_accel_data[3]) / Raw.accel_scale
         accel_scaled_z = Raw.twos_compliment((raw_accel_data[4] << 8) + raw_accel_data[5]) / Raw.accel_scale
-
         return gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z, temp
 
 
@@ -80,30 +74,22 @@ def _represent():
         #       last[1]['y'], last[1]['x'], last[1]['z'],
         #       last[2])
     except Exception, ex:
-        print ex
+        L.l.error("Unable to json accel data, ex={}".format(ex))
     return res
-
-
-def _to_json():
-    return _represent()
 
 
 class Index:
     def GET(self):
-        #print "returning {}".format(_represent())
+        L.l.info("Returning accel {}".format(_represent()))
         return _represent()
 
-
-def calibrate():
-    #Params.calibration = Params.sensor.get_all_data()
-    pass
 
 def _run_web_server():
     try:
         P.web_app = web.application(P.urls, globals())
         web.httpserver.runsimple(P.web_app.wsgifunc(), ("0.0.0.0", P.WEB_PORT))
     except Exception, ex:
-        print ex
+        L.l.error("Start accel raw data webserver failed, ex={}".format(ex))
 
 
 def unload():
@@ -118,7 +104,7 @@ def init():
     try:
         Raw.init()
     except Exception, ex:
-        print ex
+        L.l.error("Unable to init accel, ex={}".format(ex))
 
     if P.WEB_PORT != 0:
         P.web_thread = threading.Thread(target=_run_web_server)
@@ -128,16 +114,13 @@ def init():
 
 def read_sensor():
     try:
-        #sensor_all = Params.sensor.get_all_data()
         (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z,
          accel_scaled_x, accel_scaled_y, accel_scaled_z, temp) = Raw.read_all()
 
         sensor_all = [{'y': accel_scaled_y, 'x': accel_scaled_x, 'z': accel_scaled_z},
                       {'y': gyro_scaled_y, 'x': gyro_scaled_x, 'z': gyro_scaled_z}, temp]
     except Exception, ex:
-        print ex
-        sensor_all = [{'y': -0.49320554199218747, 'x': -9.528922607421874, 'z': 1.0414777221679687},
-                      {'y': -0.8625954198473282, 'x': -3.0839694656488548, 'z': 0.7938931297709924}, 99]
+        L.l.error("Unable to read accel, ex={}".format(ex))
     P.lastrecord = sensor_all
     return sensor_all
 
