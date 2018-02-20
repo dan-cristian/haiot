8a701c2cbd4464162b81c95ef6fa6e8aff907173
Dan Cristian <dan.cristian@gmail.com>
2018-02-20 23:28:02 +0200

    camera

--- apps/dashcam/recorder/__init__.py
+++ apps/dashcam/recorder/__init__.py
@@ -288,13 +288,13 @@ def _usb_init(cam):
 def _usb_init_all():
     res = _detect_usb_camera()
     if res is None or (_usb_camera_count() == _usb_is_recording_count() > 0):
-        L.l.info("exit from usb init, res={}, rec_count={}, cam_count={}".format(
-            res, _usb_is_recording_count(), _usb_camera_count()))
+        #L.l.info("exit from usb init, res={}, rec_count={}, cam_count={}".format(
+        #    res, _usb_is_recording_count(), _usb_camera_count()))
         return
     else:
         new_cam_list = res
-    L.l.info("entering usb init, res={}, rec_count={}, cam_count={}".format(
-        res, _usb_is_recording_count(), _usb_camera_count()))
+    #L.l.info("entering usb init, res={}, rec_count={}, cam_count={}".format(
+    #    res, _usb_is_recording_count(), _usb_camera_count()))
     if new_cam_list is not None:
         L.l.info("Found {} USB cameras".format(len(new_cam_list)))
     # remove gone cameras
