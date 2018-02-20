e7e2884c3e44b97df3f91d805f3588018ca6bb3f
Dan Cristian <dan.cristian@gmail.com>
2018-02-20 22:38:27 +0200

    camera

--- apps/dashcam/recorder/__init__.py
+++ apps/dashcam/recorder/__init__.py
@@ -240,13 +240,13 @@ def _detect_usb_camera():
         return None
     P.usb_last_cam_detect_attempt = datetime.datetime.now()
     new_cam_list = usb_tool.get_usb_camera_list()
-    for old_cam in P.cam_list:
-        if old_cam not in new_cam_list:
-            return new_cam_list
-    for new_cam in new_cam_list:
-        if new_cam not in P.cam_list:
-            return new_cam_list
-    return None
+    #for old_cam in P.cam_list:
+    #    if old_cam not in new_cam_list:
+    #        return new_cam_list
+    #for new_cam in new_cam_list:
+    #    if new_cam not in P.cam_list:
+    #        return new_cam_list
+    return new_cam_list
 
 
 def _start_usb_cam(cam):
@@ -287,13 +287,14 @@ def _usb_init(cam):
 
 def _usb_init_all():
     res = _detect_usb_camera()
-    if res is None or (_usb_camera_count() > 0 and _usb_is_recording_count() == _usb_camera_count()):
+    if res is None and (_usb_camera_count() == _usb_is_recording_count() > 0):
         L.l.info("exit from usb init, res={}, rec_count={}, cam_count={}".format(
             res, _usb_is_recording_count(), _usb_camera_count()))
         return
     else:
         new_cam_list = res
-    L.l.info("Found {} USB cameras".format(len(new_cam_list)))
+    if new_cam_list is not None:
+        L.l.info("Found {} USB cameras".format(len(new_cam_list)))
     # remove gone cameras
     for old_cam in list(P.cam_list):
         if old_cam not in new_cam_list:
@@ -301,14 +302,15 @@ def _usb_init_all():
             _usb_stop(old_cam)
             del P.cam_list[old_cam]
             del P.cam_param[old_cam]
-    # init existing cameras
-    for cam in new_cam_list.itervalues():
-        if cam.name not in P.cam_list:
-            L.l.info("New cam {} found".format(cam.name))
-            _usb_init(cam)
-        else:
-            L.l.info("Cam {} already initialised".format(cam.name))
-            _start_usb_cam(cam)
+    if new_cam_list is not None:
+        # init existing cameras
+        for cam in new_cam_list.itervalues():
+            if cam.name not in P.cam_list:
+                L.l.info("New cam {} found".format(cam.name))
+                _usb_init(cam)
+            else:
+                L.l.info("Cam {} already initialised".format(cam.name))
+                _start_usb_cam(cam)
 
 
 def _usb_record_loop():
