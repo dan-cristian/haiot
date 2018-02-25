248794b53d4d2d4c985f8fb516292cb4d3d235de
Dan Cristian <dan.cristian@gmail.com>
2018-02-25 14:21:16 +0200

    debug

--- apps/dashcam/recorder/__init__.py
+++ apps/dashcam/recorder/__init__.py
@@ -289,7 +289,7 @@ def _usb_init_all():
     res = _detect_usb_camera()
     if res is None or (len(res) == _usb_is_recording_count() > 0):
         L.l.info("exit from usb init, res={}, rec_count={}, cam_count={}".format(
-            res, _usb_is_recording_count(), _usb_camera_count()))
+            res, _usb_is_recording_count(), len(res)))
         return
     else:
         new_cam_list = res
