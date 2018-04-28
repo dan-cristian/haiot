7636c9cb52a117341fa925555d28748bccd0b41f
Dan Cristian <dan.cristian@gmail.com>
2018-04-28 20:37:15 +0300

    solar

--- sensor/http_parser_run.py
+++ sensor/http_parser_run.py
@@ -50,6 +50,7 @@ def thread_solar_aps_run():
                 record = models.Sensor(address=panel_id)
                 current_record = models.Sensor.query.filter_by(address=panel_id).first()
                 record.type = 'solar'
+                record.sensor_name = record.type + panel_id
                 record.temperature = temperature
                 record.updated_on = utils.get_base_location_now_date()
                 record.save_changed_fields(current_record=current_record, new_record=record,
