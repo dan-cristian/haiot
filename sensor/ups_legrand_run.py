13b8a7ecbabedddbe4a5d1225e58af846aa83fcb
Dan Cristian <dan.cristian@gmail.com>
2018-09-17 20:36:28 +0300

    usb detect

--- sensor/ups_legrand_run.py
+++ sensor/ups_legrand_run.py
@@ -3,13 +3,15 @@ __author__ = 'Dan Cristian <dan.cristian@gmail.com>'
 import time
 import serial
 from main.logger_helper import L
-from common import Constant, utils
+from common import Constant, utils, variable
 import serial_common
 from main.admin import models
 
-initialised = False
-__serial = None
-__ups = None
+
+class P:
+    initialised = False
+    serial = None
+    ups = None
 
 
 class LegrandUps:
@@ -62,7 +64,6 @@ def __write_read_port(ser, command):
 
 
 def __search_ups(port_name):
-    global __serial, __ups
     ser = serial.Serial()
     ser.port = port_name
     __open_port(ser)
@@ -73,60 +74,59 @@ def __search_ups(port_name):
             # [#                           JP00106G  #015]
             if "JP00106G" in response:
                 L.l.info('Got serial response [{}] on ups init port {}'.format(response, port_name))
-                __serial = ser
-                __ups = LegrandUps()
-                __ups.Id = str(response).replace(' ', '').replace('\r', '')
-                __ups.Name = 'Legrand Nicky ' + __ups.Id
-                __ups.Port = port_name
+                P.serial = ser
+                P.ups = LegrandUps()
+                P.ups.Id = str(response).replace(' ', '').replace('\r', '')
+                P.ups.Name = 'Legrand Nicky ' + P.ups.Id
+                P.ups.Port = port_name
                 break
             else:
                 L.l.info('Got unknown response [{}][{}] on ups init port {}'.format(
                     response, len(response), port_name))
-    if __serial is None:
+    if P.serial is None:
         ser.close()
 
 
 # (219.7 150.0 226.8 011 49.9 56.2 30.0 00001001
 def __read_ups_status():
-    global __ups, __serial
-    status = __write_read_port(__serial, 'Q1\r')
+    status = __write_read_port(P.serial, 'Q1\r')
     if status != '':
         status = status.replace('(', '')
         atoms = status.split()
         if len(atoms) >= 8:
-            __ups.InputVoltage = utils.round_sensor_value(atoms[0])
-            __ups.RemainingMinutes = utils.round_sensor_value(atoms[1])
-            __ups.OutputVoltage = atoms[2]
-            __ups.LoadPercent = utils.round_sensor_value(atoms[3])
-            __ups.PowerFrequency = atoms[4]
-            __ups.BatteryVoltage = atoms[5]
-            __ups.Temperature = atoms[6]
-            __ups.OtherStatus = atoms[7]
-            if len(__ups.OtherStatus) >= 8:
-                __ups.PowerFailed = (__ups.OtherStatus[0] == '1')
-                __ups.TestInProgress = (__ups.OtherStatus[5] == '1')
-                __ups.BeeperOn = (__ups.OtherStatus[7] == '1')
+            P.ups.InputVoltage = utils.round_sensor_value(atoms[0])
+            P.ups.RemainingMinutes = utils.round_sensor_value(atoms[1])
+            P.ups.OutputVoltage = atoms[2]
+            P.ups.LoadPercent = utils.round_sensor_value(atoms[3])
+            P.ups.PowerFrequency = atoms[4]
+            P.ups.BatteryVoltage = atoms[5]
+            P.ups.Temperature = atoms[6]
+            P.ups.OtherStatus = atoms[7]
+            if len(P.ups.OtherStatus) >= 8:
+                P.ups.PowerFailed = (P.ups.OtherStatus[0] == '1')
+                P.ups.TestInProgress = (P.ups.OtherStatus[5] == '1')
+                P.ups.BeeperOn = (P.ups.OtherStatus[7] == '1')
 
             record = models.Ups()
-            record.name = __ups.Name
+            record.name = P.ups.Name
             record.system_name = Constant.HOST_NAME
-            record.input_voltage = __ups.InputVoltage
-            record.remaining_minutes = __ups.RemainingMinutes
-            record.battery_voltage = __ups.BatteryVoltage
-            record.beeper_on = __ups.BeeperOn
-            record.load_percent = __ups.LoadPercent
-            record.output_voltage = __ups.OutputVoltage
-            record.power_frequency = __ups.PowerFrequency
-            record.temperature = __ups.Temperature
-            record.other_status = __ups.OtherStatus
-            record.power_failed = __ups.PowerFailed
+            record.input_voltage = P.ups.InputVoltage
+            record.remaining_minutes = P.ups.RemainingMinutes
+            record.battery_voltage = P.ups.BatteryVoltage
+            record.beeper_on = P.ups.BeeperOn
+            record.load_percent = P.ups.LoadPercent
+            record.output_voltage = P.ups.OutputVoltage
+            record.power_frequency = P.ups.PowerFrequency
+            record.temperature = P.ups.Temperature
+            record.other_status = P.ups.OtherStatus
+            record.power_failed = P.ups.PowerFailed
             record.updated_on = utils.get_base_location_now_date()
             current_record = models.Ups.query.filter_by(system_name=Constant.HOST_NAME).first()
             record.save_changed_fields(current_record=current_record, new_record=record, notify_transport_enabled=True,
                                        save_to_graph=True)
 
             L.l.debug('UPS remaining={} load={} input={} output={}'.format(
-                __ups.RemainingMinutes, __ups.LoadPercent, __ups.InputVoltage, __ups.OutputVoltage))
+                P.ups.RemainingMinutes, P.ups.LoadPercent, P.ups.InputVoltage, P.ups.OutputVoltage))
         else:
             L.l.warning('Unexpected number of parameters ({}) on ups status read'.format(len(atoms)))
     else:
@@ -148,15 +148,13 @@ def _create_dummy_entry():
 
 
 def unload():
-    global initialised
-    if __serial is not None and __serial.isOpen():
-        __serial.close()
-    initialised = False
+    if P.serial is not None and P.serial.isOpen():
+        P.serial.close()
+    P.initialised = False
 
 
 def init():
-    global initialised, __serial
-    initialised = False
+    P.initialised = False
     try:
         # if constant.OS in constant.OS_LINUX:
         L.l.info('Looking for serial ports')
@@ -167,23 +165,25 @@ def init():
         if len(serial_list) > 0:
             L.l.info('Looking for Legrand UPS on {} serial ports'.format(len(serial_list)))
             for device in serial_list:
-                __search_ups(device)
-                if __serial is not None:
-                    break
-            initialised = True
+                if device not in variable.USB_PORTS_IN_USE:
+                    __search_ups(device)
+                    if P.serial is not None:
+                        variable.USB_PORTS_IN_USE.append(device)
+                        P.initialised = True
+                        break
+                else:
+                    L.l.info("Skip UPS search on port {}".format(device))
         else:
             L.l.info('No standard open serial ports detected on this system')
     except Exception as ex:
         L.l.warning('Unable to open ups port, err {}'.format(ex))
-    if not initialised:
+    if not P.initialised:
         _create_dummy_entry()
-    return initialised
+    return P.initialised
 
 
 def thread_run():
-    global initialised, __serial
-    if not initialised:
+    if not P.initialised:
         init()
-
-    if initialised and __serial is not None:
+    if P.initialised and P.serial is not None:
         __read_ups_status()
