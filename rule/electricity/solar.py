f14816e2a6f431ac7880a4b664d95bfce4dbf4f7
Dan Cristian <dan.cristian@gmail.com>
2018-09-23 00:53:44 +0300

    zwave

--- rule/electricity/solar.py
+++ rule/electricity/solar.py
@@ -6,10 +6,10 @@ from rule import rule_common
 class P:
     grid_watts = 0
     plug1_watts = 0
-    PLUG1_MIN_WATTS = 10
+    PLUG1_MIN_WATTS = 20
     EXPORT_MIN_WATTS = -100
     RELAY_1_NAME = 'plug_1'
-
+    grid_importing = False
     def __init__(self):
         pass
 
@@ -23,12 +23,16 @@ def rule_energy_export(obj=models.Utility(), field_changed_list=None):
             elif obj.utility_name == 'power plug 1':
                 P.plug1_watts = obj.units_2_delta
             if P.grid_watts < 0:
-                L.l.info("Exporting power {}w".format(P.grid_watts))
+                if P.grid_importing:
+                    L.l.info("Exporting power {}w".format(P.grid_watts))
+                    P.grid_importing = False
                 if P.grid_watts < P.EXPORT_MIN_WATTS:
-                    L.l.info("Starting plug 1 additional power consumption")
+                    L.l.info("Starting plug 1 to reduce export")
                     rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=True)
             else:
-                L.l.info("Importing power {}w".format(P.grid_watts))
+                if not P.grid_importing:
+                    L.l.info("Importing power {}w".format(P.grid_watts))
+                    P.grid_importing = True
                 if P.plug1_watts > P.PLUG1_MIN_WATTS:
-                    L.l.info("Stopping plug 1 due to consumption {}w".format(P.plug1_watts))
+                    L.l.info("Stopping plug 1 due to reduce import, had {}w".format(P.plug1_watts))
                     rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=False)
