9be53cf976a6d6836eee7cd14451c2e31254bd8c
Dan Cristian <dan.cristian@gmail.com>
2018-09-23 20:17:32 +0300

    refactor sensor modules

--- rule/electricity/solar.py
+++ rule/electricity/solar.py
@@ -8,11 +8,12 @@ class P:
     grid_watts = None
     plug1_watts = None
     last_state_change = datetime.min
+    grid_importing = None
+    plug1_stopped = False
     PLUG1_MIN_WATTS = 20
     EXPORT_MIN_WATTS = -50
     RELAY_1_NAME = 'plug_1'
     STATE_CHANGE_INTERVAL = 60  # how often can change state
-    grid_importing = None
 
     def __init__(self):
         pass
@@ -38,11 +39,23 @@ def rule_energy_export(obj=models.Utility(), field_changed_list=None):
                     L.l.info("Starting plug 1 to reduce export, grid={}".format(P.grid_watts))
                     rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=True)
                     P.last_state_change = datetime.now()
+                    P.plug1_stopped = False
             else:
                 if P.grid_importing is False or P.grid_importing is None:
                     L.l.info("Importing power {}w".format(P.grid_watts))
                     P.grid_importing = True
                 if P.plug1_watts is not None and P.plug1_watts > P.PLUG1_MIN_WATTS and _can_state_change():
-                    L.l.info("Stopping plug 1 to cut import, plug {}w, grid {}w".format(P.plug1_watts, P.grid_watts))
-                    rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=False)
-                    P.last_state_change = datetime.now()
+                    power_is_on = rule_common.get_custom_relay(P.RELAY_1_NAME)
+                    if P.plug1_stopped:
+                        if power_is_on:
+                            L.l.info("Plug1 was stopped, now on, probably overriden by user, plug {}w, grid {}w".format(
+                                P.plug1_watts, P.grid_watts))
+                        else:
+                            # all ok
+                            pass
+                    else:
+                        L.l.info("Stopping plug1 to cut import, plug {}w, grid {}w".format(
+                            P.plug1_watts, P.grid_watts))
+                        rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=False)
+                        P.plug1_stopped = True
+                        P.last_state_change = datetime.now()
