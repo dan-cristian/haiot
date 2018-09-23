a778781da1df4b53f1d7406e89aab19478c172f8
Dan Cristian <dan.cristian@gmail.com>
2018-09-23 16:57:46 +0300

    refactor sensor modules

--- rule/electricity/solar.py
+++ rule/electricity/solar.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from main.logger_helper import L
 from main.admin import models
 from rule import rule_common
@@ -6,12 +7,19 @@ from rule import rule_common
 class P:
     grid_watts = 0
     plug1_watts = 0
+    last_state_change = datetime.min
     PLUG1_MIN_WATTS = 20
     EXPORT_MIN_WATTS = -50
     RELAY_1_NAME = 'plug_1'
+    STATE_CHANGE_INTERVAL = 60  # how often can change state
     grid_importing = None
+
     def __init__(self):
         pass
+
+
+def _can_state_change():
+    return (datetime.now() - P.last_state_change).total_seconds() > P.STATE_CHANGE_INTERVAL
 
 
 # energy rule
@@ -26,13 +34,15 @@ def rule_energy_export(obj=models.Utility(), field_changed_list=None):
                 if P.grid_importing is True:
                     L.l.info("Exporting power {}w".format(P.grid_watts))
                     P.grid_importing = False
-                if P.grid_watts < P.EXPORT_MIN_WATTS:
-                    L.l.info("Starting plug 1 to reduce export")
+                if P.grid_watts < P.EXPORT_MIN_WATTS and _can_state_change():
+                    L.l.info("Starting plug 1 to reduce export, grid={}".format(P.grid_watts))
                     rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=True)
+                    P.last_state_change = datetime.now()
             else:
                 if P.grid_importing is False:
                     L.l.info("Importing power {}w".format(P.grid_watts))
                     P.grid_importing = True
-                if P.plug1_watts > P.PLUG1_MIN_WATTS:
-                    L.l.info("Stopping plug 1 due to reduce import, had {}w".format(P.plug1_watts))
+                if P.plug1_watts > P.PLUG1_MIN_WATTS and _can_state_change():
+                    L.l.info("Stopping plug 1 to cut import, plug {}w, grid {}w".format(P.plug1_watts, P.grid_watts))
                     rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=False)
+                    P.last_state_change = datetime.now()
