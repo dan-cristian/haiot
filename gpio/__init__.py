72ec198b072d2d3cf4c71924be60435a9c433703
Dan Cristian <dan.cristian@gmail.com>
2018-10-13 23:08:53 +0300

    openhab

--- gpio/__init__.py
+++ gpio/__init__.py
@@ -181,7 +181,7 @@ def _process_expire():
             func = P.expire_func_list[func_time]
             L.l.info("Function expired, executing relay action func={}".format(func))
             func[0](*func[1:])
-            del func
+            del P.expire_func_list[func_time]
             L.l.info("Function deleted, list={}".format(len(P.expire_func_list)))
 
 
