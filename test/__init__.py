a5db557aa0d5ba13a62974c734a892e925c27459
Dan Cristian <dan.cristian@gmail.com>
2018-10-13 11:35:18 +0300

    zwave

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,8 @@
 __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 
 from main import L, thread_pool
-import test_run
 from main import thread_pool
+from main.admin import models
 
 initialised = False
 
@@ -16,10 +16,10 @@ def unload():
 
 def init():
     L.l.info('TEST module initialising')
-    thread_pool.add_interval_callable(test_run.thread_run, run_interval_second=60)
+    sensor_address = "ZMNHTDx Smart meter S4 S5 S6:2"
+    current_record = models.Sensor.query.filter_by(address=sensor_address).first()
+    # thread_pool.add_interval_callable(test_run.thread_run, run_interval_second=60)
     global initialised
     initialised = True
 
 
-if __name__ == '__main__':
-    test_run.thread_run()
