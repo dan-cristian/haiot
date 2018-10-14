e6b82e41856f0fe43489638c186244b0d7395bb6
Dan Cristian <dan.cristian@gmail.com>
2018-10-15 00:30:25 +0300

    sonoff

--- sensor/sonoff/__init__.py
+++ sensor/sonoff/__init__.py
@@ -1,5 +1,6 @@
 import threading
 import prctl
+from pydispatch import dispatcher
 from common import Constant, utils
 from main.logger_helper import L
 from main.admin import model_helper
@@ -16,12 +17,28 @@ class P:
         pass
 
 
+# '{"Time":"2018-10-14T21:57:33","ENERGY":{"Total":0.006,"Yesterday":0.000,"Today":0.006,"Power":5,"Factor":0.10,"Voltage":214,"Current":0.241}}'
+# 'iot/sonoff/tele/sonoff-pow-2/SENSOR'
 def mqtt_on_message(client, userdata, msg):
-    L.l.info("Client={} data={} msg={}".format(client, userdata, msg))
-
-
-def mqtt_on_message_2(client, userdata, msg):
-    L.l.info("Client2={} data={} msg={}".format(client, userdata, msg))
+    # L.l.info("Topic={} payload={}".format(msg.topic, msg.payload))
+    if '/SENSOR' in msg.topic:
+        topic_clean = P.sonoff_topic.replace('#', '')
+        if topic_clean in msg.topic:
+            sensor_name = msg.topic.split(topic_clean)[1].split('/')[1]
+            obj = utils.json2obj(msg.payload)
+            if 'ENERGY' in obj:
+                energy = obj['ENERGY']
+                power = energy['Power']
+                voltage = energy['Voltage']
+                factor = energy['Factor']
+                current = energy['Current']
+                # unit should match Utility unit name in models definition
+                dispatcher.send(Constant.SIGNAL_UTILITY_EX, sensor_name=sensor_name, value=power, unit='watt')
+                dispatcher.send(Constant.SIGNAL_UTILITY_EX, sensor_name=sensor_name, value=current, unit='kWh')
+            else:
+                L.l.warning("Energy payload missing from topic {}".format(msg.topic))
+        else:
+            L.l.warning("Invalid sensor topic {}".format(msg.topic))
 
 
 def thread_run():
@@ -39,8 +56,6 @@ def unload():
 
 def init():
     L.l.info('Sonoff module initialising')
-    P.sonoff_topic_1 = str(model_helper.get_param(Constant.P_MQTT_TOPIC_SONOFF_1))
-    P.sonoff_topic_2 = str(model_helper.get_param(Constant.P_MQTT_TOPIC_SONOFF_2))
-    mqtt_io.P.mqtt_client.message_callback_add(P.sonoff_topic_1, mqtt_on_message)
-    mqtt_io.P.mqtt_client.message_callback_add(P.sonoff_topic_2, mqtt_on_message_2)
+    P.sonoff_topic = str(model_helper.get_param(Constant.P_MQTT_TOPIC_SONOFF_1))
+    mqtt_io.P.mqtt_client.message_callback_add(P.sonoff_topic, mqtt_on_message)
     thread_pool.add_interval_callable(thread_run, P.check_period)
