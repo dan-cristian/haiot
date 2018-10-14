3e88b3b59f62f122be0eaf2d711a55add62c1d4c
Dan Cristian <dan.cristian@gmail.com>
2018-10-14 14:30:55 +0300

    solar export

--- rule/electricity/solar.py
+++ rule/electricity/solar.py
@@ -12,40 +12,100 @@ class DeviceState(Enum):
     AUTO_START = 2
     AUTO_STOP = 3
     USER_FORCED_START = 4
+    JOB_FINISHED = 5
 
 
 class Relaydevice:
     RELAY_NAME = None
-    STATE_CHANGE_INTERVAL = 300  # how often can change state
+    STATE_CHANGE_INTERVAL = 30  # how often can change state
+    MAX_OFF_INTERVAL = 600  # seconds, how long can stay off after job has started, if device supports breaks
+    DEVICE_SUPPORTS_BREAKS = False  # can this device be started/stopped several times during the job
     last_state_change = datetime.min
     state = DeviceState.NO_INIT
-    DEVICE_SUPPORTS_BREAKS = False  # can this device be started/stopped several times during the job
 
     def set_power_status(self, power_is_on):
-        rule_common.update_custom_relay(relay_pin_name=self.RELAY_NAME, power_is_on=power_is_on)
-        self.last_state_change = datetime.now()
+        if (self.state == DeviceState.USER_FORCED_START or not self.DEVICE_SUPPORTS_BREAKS) and power_is_on is False:
+            # do not stop the relay, as user forced a start
+            L.l.info("Not starting relay {}, state={}".format(self.RELAY_NAME, self.state))
+            pass
+        else:
+            if self.can_state_change():
+                rule_common.update_custom_relay(relay_pin_name=self.RELAY_NAME, power_is_on=power_is_on)
+                self.last_state_change = datetime.now()
+                if power_is_on:
+                    if self.state == DeviceState.NO_INIT or self.state == DeviceState.JOB_FINISHED:
+                        if power_is_on:
+                            self.state = DeviceState.FIRST_START
+                            L.l.info("First relay start {}, state={}".format(self.RELAY_NAME, self.state))
+                        else:
+                            self.state = DeviceState.AUTO_STOP
+                            L.l.info("Auto stop relay {}, state={}".format(self.RELAY_NAME, self.state))
+                    elif self.state == DeviceState.AUTO_STOP:
+                        self.state = DeviceState.AUTO_START
+                        L.l.info("Auto start relay {}, state={}".format(self.RELAY_NAME, self.state))
+                    elif self.state == DeviceState.AUTO_START:
+                        # already on
+                        L.l.info("Keep relay on {}, state={}".format(self.RELAY_NAME, self.state))
+                        pass
+                else:
+                    if self.state == DeviceState.AUTO_START:
+                        self.state = DeviceState.AUTO_STOP
+                        L.l.info("Auto stop relay {}, state={}".format(self.RELAY_NAME, self.state))
+                    else:
+                        L.l.info("Unexpected state relay {}, state={}".format(self.RELAY_NAME, self.state))
+            else:
+                L.l.info("Not changing relay {}, state={}".format(self.RELAY_NAME, self.state))
 
     def get_power_status(self):
         power_is_on = rule_common.get_custom_relay(self.RELAY_NAME)
+        if self.state == DeviceState.AUTO_START:
+            if power_is_on:
+                # if user forced the device start
+                self.state = DeviceState.USER_FORCED_START
         return power_is_on
 
     def can_state_change(self):
-        return (datetime.now() - self.last_state_change).total_seconds() > self.STATE_CHANGE_INTERVAL
+        return (datetime.now() - self.last_state_change).total_seconds() >= self.STATE_CHANGE_INTERVAL
 
     def __init__(self):
         pass
 
 
 class Powerdevice(Relaydevice):
-    watts = None
+    AVG_CONSUMPTION = 1
     MIN_WATTS_OFF = None  # min consumption to be considered OFF / job done
     UTILITY_NAME = None
+    JOB_FINISHED_DURATION = 180  # for how long the device stays on min consumption before job is finished
+    watts = None
+    last_min_watts_read = None
 
     def grid_updated(self, grid_watts):
-        pass
+        # get relay status to check for user forced start
+        self.get_power_status()
+        if grid_watts <= 0:
+            # start device if exporting and there is enough surplus
+            export = -grid_watts
+            if self.AVG_CONSUMPTION <= export:
+                self.set_power_status(power_is_on=True)
+                L.l.info("Should auto start device {}, state={} surplus={}".format(self.RELAY_NAME, self.state, export))
+            else:
+                self.set_power_status(power_is_on=False)
+                L.l.info("Should auto stop device {}, state={} surplus={}".format(self.RELAY_NAME, self.state, export))
+        self.update_job_finished()
 
-    def user_forced_start(self):
-        self.state = DeviceState.USER_FORCED_START
+    # check if device has finished job
+    def update_job_finished(self):
+        if self.state == DeviceState.AUTO_START:
+            if self.watts <= self.MIN_WATTS_OFF:
+                if self.last_min_watts_read is None:
+                    self.last_min_watts_read = datetime.now()
+                else:
+                    delta = (datetime.now() - self.last_min_watts_read).total_seconds()
+                    if delta >= self.JOB_FINISHED_DURATION:
+                        self.state = DeviceState.JOB_FINISHED
+                        self.last_min_watts_read = None
+            else:
+                self.last_min_watts_read = None
 
     def set_watts(self, watts):
         self.watts = watts
@@ -55,10 +115,12 @@ class Powerdevice(Relaydevice):
 
 
 class Dishwasher(Powerdevice):
+    AVG_CONSUMPTION = 80
     MIN_WATTS_OFF = 1
     RELAY_NAME = 'plug_1'
     UTILITY_NAME = 'power plug 1'
     DEVICE_SUPPORTS_BREAKS = True
+    MAX_OFF_INTERVAL = 60 * 30  # until water get's cold
 
     def __init__(self):
         Powerdevice.__init__(self)
@@ -66,17 +128,20 @@ class Dishwasher(Powerdevice):
 
 
 class Washingmachine(Powerdevice):
+    AVG_CONSUMPTION = 80
     MIN_WATTS_OFF = 2
     RELAY_NAME = 'plug_2'
     UTILITY_NAME = 'power plug 2'
     DEVICE_SUPPORTS_BREAKS = False
+    MAX_OFF_INTERVAL = 60 * 10  # until water get's cold
 
     def __init__(self):
         Powerdevice.__init__(self)
 
 
 class Upscharger(Relaydevice):
-    RELAY_NAME = 'ups_charger'
+    AVG_CONSUMPTION = 250
+    RELAY_NAME = 'beci_upscharge_relay'
     DEVICE_SUPPORTS_BREAKS = False
 
     def __init__(self):
@@ -96,69 +161,15 @@ class P:
         self.device_list[Upscharger.RELAY_NAME] = Upscharger()
 
 
-
-
-# will I have export without this extra load?
-def _exporting(extra_load):
-    if extra_load is None:
-        extra_load = 0
-    if P.grid_watts is not None:
-        return P.grid_watts - extra_load < P.EXPORT_MIN_WATTS
-    else:
-        return False
-
-
 # energy rule
 def rule_energy_export(obj=models.Utility(), field_changed_list=None):
-    if field_changed_list is not None:
-        if 'units_2_delta' in field_changed_list:
-            if obj.utility_name == 'power main mono':
-                P.grid_watts = obj.units_2_delta
-                for device in P.device_list:
-                    device.grid_updated(P.grid_watts)
-            else:
-                if obj.utility_name in P.device_list.keys():
-                    P.device_list[obj.utility_name].set_watts(obj.units_2_delta)
-
-    return
-    # if exporting
-    if P.grid_watts is not None:
-        if _exporting(P.plug1_watts):
-            if P.grid_importing is True or P.grid_importing is None:
-                L.l.info("Exporting power {}w".format(P.grid_watts))
-                P.grid_importing = False
-            if _can_state_change() and not P.plug1_job_started:
-                L.l.info("Starting plug 1 to reduce export, grid={}".format(P.grid_watts))
-                rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=True)
-                P.last_state_change = datetime.now()
-                # P.plug1_auto_stopped = False
-                P.plug1_job_started = True
-                return
+    if field_changed_list is not None and 'units_2_delta' in field_changed_list:
+        if obj.utility_name == 'power main mono':
+            P.grid_watts = obj.units_2_delta
+            # let all devices know grid status
+            for device in P.device_list:
+                device.grid_updated(P.grid_watts)
         else:
-            if P.grid_importing is False or P.grid_importing is None:
-                L.l.info("Importing power {}w".format(P.grid_watts))
-                P.grid_importing = True
-            if P.plug1_watts is not None and P.plug1_watts > P.PLUG1_MIN_WATTS_ON and _can_state_change():
-                power_is_on = rule_common.get_custom_relay(P.RELAY_1_NAME)
-                if P.plug1_auto_stopped is True:
-                    if power_is_on:
-                        L.l.info("Plug1 started, probably overriden by user, plug {}w, grid {}w".format(
-                            P.plug1_watts, P.grid_watts))
-                        # to supress above info messages
-                        P.last_state_change = datetime.now()
-                    else:
-                        # all ok, plug is stopped, power is off, saving!
-                        pass
-                else:
-                    L.l.info("Stopping plug1 to cut import, plug {}w, grid {}w".format(
-                        P.plug1_watts, P.grid_watts))
-                    time.sleep(10)
-                    rule_common.update_custom_relay(relay_pin_name=P.RELAY_1_NAME, power_is_on=False)
-                    P.plug1_auto_stopped = True
-                    P.last_state_change = datetime.now()
-            # reset user override when done to enable automatic switch
-            # fixme: min watts might go below in the process, check multiple values. enters here after plug start, avoid!
-            if P.plug1_job_started and P.plug1_watts is not None and P.plug1_watts <= P.PLUG1_MIN_WATTS_OFF:
-                P.plug1_auto_stopped = False
-                L.l.info("Plug1 no more consumption, job done, plug {}w, grid {}w".format(P.plug1_watts, P.grid_watts))
-                P.plug1_job_started = False
+            # set consumption for device
+            if obj.utility_name in P.device_list.keys():
+                P.device_list[obj.utility_name].set_watts(obj.units_2_delta)
