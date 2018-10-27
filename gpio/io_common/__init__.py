d45d9d9647dfcccd4e52b42a6c3902046fec997f
Dan Cristian <dan.cristian@gmail.com>
2018-10-28 00:42:30 +0300

    piface fix

--- gpio/io_common/__init__.py
+++ gpio/io_common/__init__.py
@@ -18,4 +18,6 @@ def update_custom_relay(pin_code, pin_value, notify=False):
         relay.notify_transport_enabled = notify
         relay.commit_record_to_db()
         L.l.info('Updated relay {} val={}'.format(pin_code, pin_value))
+    else:
+        L.l.warning('Unable to find relay pin {}'.format(pin_code))
 
