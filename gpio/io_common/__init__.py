d1574d677d33eaab46712c2a8164605316dd7b33
Dan Cristian <dan.cristian@gmail.com>
2018-10-27 21:21:55 +0300

    piface fix

--- gpio/io_common/__init__.py
+++ gpio/io_common/__init__.py
@@ -3,14 +3,14 @@ from main.admin import models
 
 
 # update in db without propagatting the change
-def update_custom_relay(pin_code, pin_value):
+def update_custom_relay(pin_code, pin_value, notify=False):
     gpio = models.GpioPin.query.filter_by(pin_code=pin_code, host_name=Constant.HOST_NAME).first()
     if gpio is not None:
         gpio.pin_value = int(pin_value)
-        gpio.notify_transport_enabled = False
+        gpio.notify_transport_enabled = notify
         gpio.commit_record_to_db()
     relay = models.ZoneCustomRelay.query.filter_by(gpio_pin_code=pin_code, gpio_host_name=Constant.HOST_NAME).first()
     if relay is not None:
         relay.relay_is_on = bool(pin_value)
-        relay.notify_transport_enabled = False
+        relay.notify_transport_enabled = notify
         relay.commit_record_to_db()
