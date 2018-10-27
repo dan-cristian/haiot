6437110dd3b3319d3c4b1d172c2754d3127057b6
Dan Cristian <dan.cristian@gmail.com>
2018-10-27 21:54:15 +0300

    piface fix

--- ddns/__init__.py
+++ ddns/__init__.py
@@ -4,19 +4,22 @@ from main.logger_helper import L
 from main import thread_pool
 import ddns_run
 
-initialised = False
+
+class P:
+    initialised = False
+
+    def __init__(self):
+        pass
 
 
 def unload():
     L.l.info('DDNS module unloading')
     # ...
     thread_pool.remove_callable(ddns_run.thread_run)
-    global initialised
-    initialised = False
+    P.initialised = False
 
 
 def init():
     L.l.info('DDNS module initialising')
     thread_pool.add_interval_callable(ddns_run.thread_run, run_interval_second=600)
-    global initialised
-    initialised = True
+    P.initialised = True
