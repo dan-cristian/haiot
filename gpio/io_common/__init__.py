01657123fa568de927c023c7cf6427308f07e830
Dan Cristian <dan.cristian@gmail.com>
2018-10-27 23:49:07 +0300

    piface fix

--- gpio/io_common/__init__.py
+++ gpio/io_common/__init__.py
@@ -1,5 +1,6 @@
 from common import Constant
 from main.admin import models
+from main.logger_helper import L
 
 
 # update in db without propagatting the change
@@ -9,8 +10,12 @@ def update_custom_relay(pin_code, pin_value, notify=False):
         gpio.pin_value = int(pin_value)
         gpio.notify_transport_enabled = notify
         gpio.commit_record_to_db()
+    else:
+        L.l.warning('Unable tp find gpio pin {}'.format(pin_code))
     relay = models.ZoneCustomRelay.query.filter_by(gpio_pin_code=pin_code, gpio_host_name=Constant.HOST_NAME).first()
     if relay is not None:
         relay.relay_is_on = bool(pin_value)
         relay.notify_transport_enabled = notify
         relay.commit_record_to_db()
+    else:
+        L.l.warning('Unable tp custom relay pin {}'.format(pin_code))
