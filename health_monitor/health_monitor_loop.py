8786817e59a86d5340dacb445ed9149e4d4b740a
Dan Cristian <dan.cristian@gmail.com>
2018-11-17 00:53:46 +0200

    power ina

--- health_monitor/health_monitor_loop.py
+++ health_monitor/health_monitor_loop.py
@@ -538,7 +538,7 @@ def _read_battery_power():
                                 voltage=voltage, current=current, power=power)
                 power_rec = models.PowerMonitor.query.filter_by(name=addr[0]).first()
                 if power_rec is not None:
-                    new_rec = models.PowerMonitor()
+                    new_rec = models.PowerMonitor(id=power_rec.id)
                     new_rec.voltage = voltage
                     new_rec.current = current
                     new_rec.power = power
