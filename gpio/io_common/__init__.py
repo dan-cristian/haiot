3b9adc8f291b8e4c23babf8bb570c11d93415b5c
Dan Cristian <dan.cristian@gmail.com>
2018-12-24 00:14:45 +0200

    piface

--- gpio/io_common/__init__.py
+++ gpio/io_common/__init__.py
@@ -4,14 +4,15 @@ from main.logger_helper import L
 
 
 # update in db without propagatting the change
-def update_custom_relay(pin_code, pin_value, notify=False):
+def update_custom_relay(pin_code, pin_value, notify=False, ignore_missing=False):
     gpio = models.GpioPin.query.filter_by(pin_code=pin_code, host_name=Constant.HOST_NAME).first()
     if gpio is not None:
         gpio.pin_value = int(pin_value)
         gpio.notify_transport_enabled = notify
         gpio.commit_record_to_db()
     else:
-        L.l.warning('Unable to find gpio pin {}'.format(pin_code))
+        if not ignore_missing:
+            L.l.warning('Unable to find gpio pin {}'.format(pin_code))
     relay = models.ZoneCustomRelay.query.filter_by(gpio_pin_code=pin_code, gpio_host_name=Constant.HOST_NAME).first()
     if relay is not None:
         relay.relay_is_on = pin_value
@@ -19,5 +20,6 @@ def update_custom_relay(pin_code, pin_value, notify=False):
         relay.commit_record_to_db()
         L.l.info('Updated relay {} val={}'.format(pin_code, pin_value))
     else:
-        L.l.warning('Unable to find relay pin {}'.format(pin_code))
+        if not ignore_missing:
+            L.l.warning('Unable to find relay pin {}'.format(pin_code))
 
