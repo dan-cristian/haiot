3452555151127738e0c4cf3ad95f66f28a80698e
Dan Cristian <dan.cristian@gmail.com>
2019-01-01 19:06:55 +0200

    piface

--- gpio/piface/__init__.py
+++ gpio/piface/__init__.py
@@ -83,14 +83,16 @@ def _setup_in_ports_pif(gpio_pin_list):
                 if board in P.listener.keys():
                     P.listener[board].register(pin, pfio.IODIR_ON, _input_event)
                     P.listener[board].register(pin, pfio.IODIR_OFF, _input_event)
-                    P.listener[board].register(pin, pfio.IODIR_BOTH, _input_event)
+                    # P.listener[board].register(pin, pfio.IODIR_BOTH, _input_event)
                     L.l.info('OK callback set on piface board {}, {} pin {}'.format(board, gpio_pin.pin_code, pin))
                     # _read_default(pin=pin, board_index=board)
             except Exception as ex:
                 L.l.critical('Unable to setup piface listener board={} pin={} err={}'.format(
                     board, gpio_pin.pin_code, ex))
+    L.l.info('Activating listeners')
     for li in P.listener.values():
         li.activate()
+    L.l.info('Activating listeners done')
 
 
 def _setup_board():
