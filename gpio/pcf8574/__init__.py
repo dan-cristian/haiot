295b5ed8edf492017176bf5a644923acf20b1cac
Dan Cristian <dan.cristian@gmail.com>
2019-01-13 02:03:40 +0200

    temp fix

--- gpio/pcf8574/__init__.py
+++ gpio/pcf8574/__init__.py
@@ -1,8 +1,25 @@
-from pcf8574 import PCF8574
+from main.logger_helper import L
 
-def init():
+
+class P:
+    pcf = None
     i2c_port_num = 1
     pcf_address = 0x24
-    pcf = PCF8574(i2c_port_num, pcf_address)
-    pcf.port[0] = False
-    print pcf.port
+    import_module_exist = None
+
+    def __init__(self):
+        pass
+
+
+try:
+    from pcf8574 import PCF8574
+    P.import_module_exist = True
+except ImportError:
+    L.l.info('Module PCF8574 is not installed, module will not be initialised')
+
+
+def init():
+    P.pcf = PCF8574(P.i2c_port_num, P.pcf_address)
+
+pcf.port[0] = False
+print pcf.port
