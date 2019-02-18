fd798d9352262ab0023f13b33df5f0901766e53b
Dan Cristian <dan.cristian@gmail.com>
2019-02-18 02:22:52 +0200

    pcf

--- haiot.py
+++ haiot.py
@@ -1,8 +1,9 @@
 __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
 import sys
 
-#http://www.tutorialspoint.com/python/python_command_line_arguments.htm
+# http://www.tutorialspoint.com/python/python_command_line_arguments.htm
 
 if __name__ == '__main__':
     import main
+    sys.settrace(None)
     main.run(sys.argv[1:])
