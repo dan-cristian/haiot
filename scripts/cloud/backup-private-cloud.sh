1b2edd28e6703df715c41efb9731837800612781
Dan Cristian <dan.cristian@gmail.com>
2019-02-26 01:34:33 +0200

    scripts

--- scripts/cloud/backup-private-cloud.sh
+++ scripts/cloud/backup-private-cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOG=/mnt/log/backup-private-cloud.log
-. ${HAIOT_DIR}/.credentials/.general.credentials
+. /home/haiot/PYC/.credentials/.general.credentials
 
 function echo2(){
 echo [`date +%T.%N`] $1 $2 $3 $4 $5 >> $LOG 2>&1
