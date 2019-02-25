2723d2beaeabf85762423411823abc115a1473f9
Dan Cristian <dan.cristian@gmail.com>
2019-02-25 22:01:21 +0200

    scripts

--- scripts/camera/recordalert.sh
+++ scripts/camera/recordalert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #parameters 1:zoneid, 2:message
-
+source ${HAIOT_DIR}/.credentials/.general.credentials
 #ignore very fast requests, allow just one every 30 seconds
 savefile=/tmp/lastalertdate$1.txt
 touch $savefile
@@ -18,5 +18,5 @@ if [ $diffsec -le 30 ] ; then
 else
 	echo $datenow > $savefile
 	logger "Motion alerting cam $1 $2 $3"
-	wget -q --output-document=/dev/null --timeout=1 --tries=1 "http://192.168.0.12:8080/apiv1/camera_alert/zone_name=$1&cam_name=$2&has_move=$3"
+	wget -q --output-document=/dev/null --timeout=1 --tries=1 ${HAIOT_API_URL}"/apiv1/camera_alert/zone_name=$1&cam_name=$2&has_move=$3"
 fi
