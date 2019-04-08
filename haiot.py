8cc46b5ba89b8d188b1119888cf763feafa5cf59
Dan Cristian <dan.cristian@gmail.com>
2019-04-09 01:28:32 +0300

    tinydb refactoring

--- haiot.py
+++ haiot.py
@@ -1,8 +1,20 @@
 __author__ = 'Dan Cristian<dan.cristian@gmail.com>'
+import signal
 import sys
 
+
 # http://www.tutorialspoint.com/python/python_command_line_arguments.htm
+def signal_handler(signal_name, frame):
+    print('I got signal {} frame {}, exiting'.format(signal_name, frame))
+    main.tinydb_app.unload()
+
 
 if __name__ == '__main__':
-    import main
-    main.run(sys.argv[1:])
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    import main.general_init
+    main.general_init.init()
+
+    import main.tinydb_app
+    main.tinydb_app.init(sys.argv[1:])
+    #main.run(sys.argv[1:])
