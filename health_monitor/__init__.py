bd42e810b9d11932e705126270f9dc691beb5577
Dan Cristian <dan.cristian@gmail.com>
2019-04-17 23:28:26 +0300

    tinydb fix

--- health_monitor/__init__.py
+++ health_monitor/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'dcristian'
 
 from main import thread_pool
-import health_monitor_loop
+from health_monitor import health_monitor_loop
 from main.logger_helper import L
 
 initialised=False
@@ -22,5 +22,5 @@ def init():
     health_monitor_loop.init()
     thread_pool.add_interval_callable(func=health_monitor_loop.thread_run, run_interval_second=10)
     global initialised
-    #health_monitor_loop.thread_run()
+    # health_monitor_loop.thread_run()
     initialised = True
