09fe9ca0b71cf0167f3bc2780948c72003ca6ffc
Dan Cristian <dan.cristian@gmail.com>
2019-04-23 08:12:12 +0300

    python3

--- music/gmusicproxy/__init__.py
+++ music/gmusicproxy/__init__.py
@@ -1,18 +1,19 @@
 from main.logger_helper import L
 # from main.admin.model_helper import get_param
 from common import Constant, get_json_param
-import urllib.request
+import requests
+import urllib.parse
 
 
 # http://gmusicproxy.net/
 
 def get_song_id(artist, title):
     url = get_json_param(Constant.P_GMUSICPROXY_URL)
-    title = urllib.quote(title.encode('utf-8'), safe='')
-    artist = urllib.quote(artist.encode('utf-8'), safe='')
+    title = urllib.parse.quote(title.encode('utf-8'), safe='')
+    artist = urllib.parse.quote(artist.encode('utf-8'), safe='')
     param = '/search_id?type=song&title={}&artist={}&exact=yes'.format(title, artist)
     try:
-        result = urllib.requests.get('{}{}'.format(url, param), timeout=7).text
+        result = requests.get('{}{}'.format(url, param), timeout=7).text
         if result == '':
             return None
         else:
