6b88a2c0d09db13010be56211303f9edb964684b
Dan Cristian <dan.cristian@gmail.com>
2019-05-02 20:50:52 +0300

    heat

--- heat/__init__.py
+++ heat/__init__.py
@@ -149,12 +149,16 @@ def _get_heat_on_keep_warm(schedule_pattern, temp_code, temp_target, temp_actual
                 force_on = ((schedule_pattern.keep_warm_pattern[interval] == "1") and
                             temp_code is not P.TEMP_NO_HEAT)
                 if force_on:
-                    L.l.info("Forcing heat on due to keep warm, zone {}".format(schedule_pattern.name))
+                    L.l.info("Forcing heat on keep warm, zone {} interval {} pattern {}".format(
+                        schedule_pattern.name, interval, schedule_pattern.keep_warm_pattern[interval]))
             else:
                 L.l.info("Temp too high in {} with {}, ignoring keep warm".format(schedule_pattern.name, delta_warm))
         else:
             L.l.critical("Missing or incorrect keep warm pattern for zone {}={}".format(
                 schedule_pattern.name, schedule_pattern.keep_warm_pattern))
+    else:
+        L.l.info('Keep warm off {} pattern.keep {} temp_act {}'.format(
+            schedule_pattern.name, schedule_pattern.keep_warm, temp_actual))
     return force_on
 
 
