3d9b1bfe63d26b056da829c45c3834b815ec2f44
Dan Cristian <dan.cristian@gmail.com>
2019-05-03 22:20:23 +0300

    piface io

--- gpio/rpi_gpio/__init__.py
+++ gpio/rpi_gpio/__init__.py
@@ -98,8 +98,8 @@ def _event_detected_falling(channel):
 
 
 def _event_detected_both(channel):
-    # now_state = GPIO.input(channel)
-    # L.l.info("Both event, channel {}, now_state={}".format(channel, now_state))
+    now_state = GPIO.input(channel)
+    L.l.info("Both event, channel {}, now_state={}".format(channel, now_state))
     # time.sleep(0.1)
     new_state = GPIO.input(channel)
     L.l.info("Both event, channel {}, NEW_state={}".format(channel, new_state))
@@ -107,11 +107,12 @@ def _event_detected_both(channel):
 
 
 def _event_detected_reversed_both(channel):
-    # now_state = GPIO.input(channel)
+    now_state = GPIO.input(channel)
+    L.l.info("Both event reversed, channel {}, now_state={}".format(channel, now_state))
     # time.sleep(0.1)
     new_state = GPIO.input(channel)
     # reverse state for normal open contacts
-    # L.l.info("State pin before reverse={}".format(new_state))
+    L.l.info("State pin before reverse={}".format(new_state))
     rev_state = int(not new_state)
     L.l.info("State pin {} after reverse={}".format(channel, rev_state))
     _do_event(channel, rev_state)
