2f1cadb1a518db4a9a8621db46a590d3e6783a65
Dan Cristian <dan.cristian@gmail.com>
2019-05-04 12:50:52 +0300

    piface io

--- sensor/rfxcom/__init__.py
+++ sensor/rfxcom/__init__.py
@@ -133,7 +133,7 @@ def _init_board():
             L.l.info('No RFX device detected on this system')
     except Exception as ex:
         L.l.warning('Unable to open RFX tty port, err={}'.format(ex))
-    if not P.initialised:
+    if P.initialised is False:
         P.init_failed_count += 1
     return P.initialised
 
