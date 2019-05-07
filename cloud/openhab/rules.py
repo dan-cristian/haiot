67987e254bd1a7217aee9b7b27cdb50f49adc9f7
Dan Cristian <dan.cristian@gmail.com>
2019-05-07 23:39:42 +0300

    openhab events rewrite

--- cloud/openhab/rules.py
+++ cloud/openhab/rules.py
@@ -2,7 +2,7 @@ import transport.mqtt_io
 from main.logger_helper import L
 from storage.model import m
 import transport
-from common import Constant
+from common import Constant, utils
 
 
 class P:
