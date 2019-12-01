7528448350fcd68212de6f714357c5a8a7641b8b
Dan Cristian <dan.cristian@gmail.com>
2019-12-01 14:11:55 +0200

    fix sync

--- main/event.py
+++ main/event.py
@@ -46,7 +46,8 @@ def _process_obj(obj):
                 # cls = getattr(sys.modules[tinydb_model.__name__], table)
                 cls = getattr(m, table)
                 # if cls._is_used_in_module:
-                if 'Pwm' in table and Constant.HOST_NAME == 'pizero1':
+                if ('Pwm' in table or 'ZoneCustomRelay' in table) and (
+                        Constant.HOST_NAME == 'pizero1' or Constant.HOST_NAME == 'pi-power'):
                     L.l.info('Got mqtt {}'.format(obj))
                 cls.save(obj)
                 # else:
