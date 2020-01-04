ad0882a6904782339beecc590df1f0cbb43816ab
Dan Cristian <dan.cristian@gmail.com>
2020-01-05 01:26:26 +0200

    conf fix

--- scripts/cloud/backup.sh
+++ scripts/cloud/backup.sh
@@ -3,15 +3,16 @@
 DATE=`date +%Y%m%d-%H%M%S`
 HOST=`cat /etc/hostname`
 BACKUP_DIR=/mnt/backup
+BACKUP_DIFF_DIR=/mnt/backup_rdiff
 FILE_BACKUP=$BACKUP_DIR/$HOST-Backup$DATE.tar.gz
 
 echo Backing up personal data with rdiff
 sleep 5
 
-rdiff-backup /mnt/data/hdd-wdg-6297/photos $BACKUP_DIR/photos.backup
-rdiff-backup /mnt/data/hdd-wdg-6297/private $BACKUP_DIR/private.backup
-rdiff-backup /mnt/data/hdd-wdg-6297/ebooks $BACKUP_DIR/ebooks.backup
-rdiff-backup /mnt/data/hdd-wdr-evhk/videos $BACKUP_DIR/videos.backup
+rdiff-backup /mnt/data/hdd-wdg-6297/photos $BACKUP_DIFF_DIR/photos.backup
+rdiff-backup /mnt/data/hdd-wdg-6297/private $BACKUP_DIFF_DIR/private.backup
+rdiff-backup /mnt/data/hdd-wdg-6297/ebooks $BACKUP_DIFF_DIR/ebooks.backup
+rdiff-backup /mnt/data/hdd-wdr-evhk/videos $BACKUP_DIFF_DIR/videos.backup
 
 echo Backing up system to $FILE_BACKUP
 sleep 5
