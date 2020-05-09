c4d8aa0e6f59a9f7623bc4f75ebc35a097ec54ad
Dan Cristian <dan.cristian@gmail.com>
2020-05-09 15:50:43 +0300

    micropython

--- apps/esp8266_pwm_load/main1.py
+++ apps/esp8266_pwm_load/main1.py
@@ -1,6 +1,8 @@
 import wifi
 import mqtt
 import pwm
+import time
+import machine
 from credentials import ssid, password, mqtt_pass, mqtt_user
 
 pwm_pin = 13
@@ -12,10 +14,20 @@ topic_sub = "iot/main/#"
 topic_pub = "iot/sonoff/" + client_id + "/"
 
 
+def restart_and_reconnect():
+    print('Restarting ESP...')
+    time.sleep(5)
+    machine.reset()
+
+
 def main():
     wifi.connect(ssid, password)
     pwm.init(pin=pwm_pin, frequency=pwm_frequency)
-    mqtt.connect(client_id, mqtt_server, topic_sub, topic_pub, user=mqtt_user, password=mqtt_pass)
+    result = mqtt.connect(client_id, mqtt_server, topic_sub, topic_pub, user=mqtt_user, password=mqtt_pass)
+    if result == 'restart':
+        restart_and_reconnect()
+    else:
+        print("Unknown action")
 
 
 if __name__ == '__main__':
