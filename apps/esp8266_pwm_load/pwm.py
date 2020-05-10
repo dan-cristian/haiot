a33a358e70ace7437238a3da745ac8faa1ae9168
Dan Cristian <dan.cristian@gmail.com>
2020-05-10 03:07:26 +0300

    micropython

--- apps/esp8266_pwm_load/pwm.py
+++ apps/esp8266_pwm_load/pwm.py
@@ -1,5 +1,6 @@
 import machine
 
+pin_code = None
 pin_pwm = None
 
 # https://docs.micropython.org/en/latest/esp8266/tutorial/pwm.html
@@ -7,16 +8,28 @@ pin_pwm = None
 
 def set_duty(duty):
     global pin_pwm
+    if pin_pwm is None:
+        print("Pwm not yet initialised")
+        return
     # expect duty as a %
     pin_pwm.duty(int(1023 * duty/100))
 
 
 def set_frequency(frequency):
     global pin_pwm
+    if pin_pwm is None:
+        print("Pwm not yet initialised")
+        return
     pin_pwm.freq(frequency)
 
 
+def set_pin(pin):
+    if pin_code != pin:
+        init(pin, 0)
+
+
 def init(pin, frequency):
-    global pin_pwm
+    global pin_pwm, pin_code
     pin_pwm = machine.PWM(machine.Pin(pin), frequency)
     pin_pwm.duty(0)
+    pin_code = pin
