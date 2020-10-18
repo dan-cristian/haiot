8bf2563be0788ae8ca270192a2d437ba60eb4612
Dan Cristian <dan.cristian@gmail.com>
2020-10-18 23:14:08 +0300

    alarms

--- rule/lights/__init__.py
+++ rule/lights/__init__.py
@@ -5,6 +5,6 @@ from storage.model import m
 
 def rule_alarm(obj=m.ZoneAlarm(), change=None):
     if obj.alarm_pin_triggered:
-        if obj.target_relay is not None:
+        if hasattr(obj, 'target_relay') and obj.target_relay is not None:
             # switch on relay
             rule_common.update_custom_relay(obj.target_relay, True)
