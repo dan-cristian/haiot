7a93d4e284a89e0da31056f5ee9b80ba90e8ea5b
Dan Cristian <dan.cristian@gmail.com>
2021-01-31 18:27:24 +0200

    db

--- scripts/cloud/backup-private-cloud.sh
+++ scripts/cloud/backup-private-cloud.sh
@@ -9,7 +9,7 @@ echo [`date +%T.%N`] $1 $2 $3 $4 $5
 
 function backup(){
 # rsync -avrPe 'ssh -p 222 -T -c aes128-cbc -o Compression=no -x ' $1 haiot@$HOST_DEST:/media/usb/$2
-rsync -avrPe 'ssh -T -p '${BACKUP_SSH_PORT}' -c '${BACKUP_SSH_CIPHER}' -o Compression=no -x' $1 ${BACKUP_SSH_SERVER}:${BACKUP_PATH}/$2 >> $LOG 2>&1
+rsync -avrPe 'ssh -T -p '${BACKUP_SSH_PORT}' -o Compression=no -x' $1 ${BACKUP_SSH_SERVER}:${BACKUP_PATH}/$2 >> $LOG 2>&1
 }
 
 echo2 'Starting backup to private cloud '${BACKUP_SSH_SERVER}' '${BACKUP_SSH_PORT}' '${BACKUP_SSH_CIPHER}
