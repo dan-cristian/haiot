360ee56b89aa23b2d2cda9dd86c9659251798adf
Dan Cristian <dan.cristian@gmail.com>
2021-02-15 21:11:49 +0200

    vent dynamic

--- cloud/openhab/rules.py
+++ cloud/openhab/rules.py
@@ -80,6 +80,18 @@ def rule_openhab_ventilation(obj=m.Ventilation(), change=None):
                     L.l.warning('Field {} in ventilation change list but not in obj={}'.format(key, obj))
 
 
+def rule_openhab_vent(obj=m.Vent(), change=None):
+    if change is not None:
+        name = obj.name
+        for key in change:
+            if key not in P.ignored_fields:
+                if hasattr(obj, key):
+                    val = getattr(obj, key)
+                    send_mqtt_openhab(subtopic='vent_' + key + '_' + name, payload=val)
+                else:
+                    L.l.warning('Field {} in vent change list but not in obj={}'.format(key, obj))
+
+
 def rule_openhab_utility(obj=m.Utility(), change=None):
     if hasattr(obj, 'utility_type') and hasattr(obj, 'utility_name') and obj.utility_name is not None:
         # L.l.info("PROCESSING utility {}".format(obj.utility_type))
