e9a58c33f707c1bdca6771bb3c0111aab77a1577
Dan Cristian <dan.cristian@gmail.com>
2026-02-15 21:28:20 +0200

    ha

--- cloud/ha/__init__.py
+++ cloud/ha/__init__.py
@@ -4,12 +4,13 @@ from main.logger_helper import L
 from main import thread_pool
 from pydispatch import dispatcher
 from common import Constant, get_json_param
-from inspect import getmembers, isfunction
 from transport import mqtt_io
 import transport
 import threading
 import prctl
+import common
 from storage.dicts.model import HADiscoverableDevice
+from storage.dicts import model
 
 class P:
     event_list = []
@@ -20,6 +21,7 @@ class P:
     mqtt_topic_receive_prefix = None
     discovery_timestamps = {}
     discovery_resent_period = 60*60
+    availability_timestamps = {}
 
     # https://www.home-assistant.io/integrations/mqtt/#sensors
     discovery_template_1=('{"unique_id":"<unique_id>",' +
@@ -44,84 +46,53 @@ class P:
     def __init__(self):
         pass
 
+class TemplateSensorDiscovery:
+    """{
+    "name":"<sensor_name>",
+    "unique_id":"<unique_sensor_id>",
+    "availability_topic":"<availability_topic>",
+    "state_topic":"<state_topic>",
+    "device":{
+        "name":"<device_name>",
+        "identifiers":["<unique_device_id>"],
+        "model":"haiot"
+        }
+    ,"device_class":"<device_class>",
+    "unit_of_measurement":"<unit_of_measurement>"
+    }"""
+    availability_topic = '<ha_mqtt_prefix><device_name>/status'
+    state_topic = '<ha_mqtt_prefix><device_type>/<unique_sensor_id>/state'
+
+def _get_variables(obj):
+    device_name = type(obj).__name__ + "_" + "{}".format(getattr(obj, obj._main_key)).replace(":", "-")
+    device_name = device_name.replace(' ', '_')
+    # https://www.home-assistant.io/integrations/mqtt/#discovery-payload
+    if hasattr(obj, "ha_device_type"):
+        device_type = obj.ha_device_type
+    else:
+        device_type = "sensor"
+    availability_topic = TemplateSensorDiscovery.availability_topic.replace(
+        "<ha_mqtt_prefix>", P.ha_topic).replace(
+        "<device_name>", device_name)
+    return device_name, device_type, availability_topic
+
 def parse_rules(obj, change):
     """ running rule method corresponding with obj type """
     # executed on all db value changes
-    if isinstance(obj, HADiscoverableDevice):
-        device_name = type(obj).__name__ + "_" + "{}".format(getattr(obj, obj._main_key)).replace(":", "-")
-        device_name = device_name.replace(' ', '_')
-        # publish a discoverable package periodically
-        # https://www.home-assistant.io/integrations/mqtt/#discovery-payload
-        if hasattr(obj, "ha_device_type"):
-            device_type = obj.ha_device_type
-        else:
-            device_type = "sensor"
+    device_name, device_type, availability_topic = _get_variables(obj)
+    for item in change:
+        if item != 'updated_on' and item != 'source_host':
+            sensor_unique_id = device_name + '_' + item
+            subtopic = "{}/{}/state".format(device_type, sensor_unique_id)
+            value = "{}".format(getattr(obj, item))
+            # payload = P.sensor_template.replace("<device_class>", item).replace("<sensor_value>", value)
+            payload = value
+            send_mqtt_ha(topic=subtopic, payload=payload)
+            send_mqtt_ha(topic=availability_topic, payload="online")
+            P.availability_timestamps[device_name] = datetime.datetime.now()
 
-        if device_name in P.discovery_timestamps.keys():
-            last_discovery = P.discovery_timestamps[device_name]
-        else:
-            last_discovery = datetime.datetime.min
-        if (datetime.datetime.now() - last_discovery).total_seconds() > P.discovery_resent_period:
-            if hasattr(obj, "ha_device_class") and hasattr(obj, "ha_device_class_unit"):
-                ha_field_list = obj.ha_fields.strip().split(',')
-                device_class_list = obj.ha_device_class.strip().split(',')
-                device_class_unit_list = obj.ha_device_class_unit.strip().split(',')
-                index = 0
-                for ha_field in ha_field_list:
-                    device_class = device_class_list[index]
-                    device_unit = device_class_unit_list[index]
-                    device_unique_id = device_name + '_' + ha_field
-                    subtopic = "{}/{}/config".format(device_type, device_unique_id)
-                    payload = P.discovery_template.replace(
-                        "<unique_id>", device_unique_id).replace(
-                        "<device_class>", device_class).replace(
-                        "<unit_of_measurement>", device_unit).replace(
-                        "<ha_mqtt_prefix>", P.ha_topic).replace(
-                        "<device_type>", device_type).replace(
-                        "<device_identifier_name>", device_name).replace(
-                        "<location_name>", device_name)
-                    if '"device_class":"",' in payload:
-                        payload = payload.replace('"device_class":"",','').replace(
-                            '"unit_of_measurement":"",','')
-                    send_mqtt_ha(subtopic=subtopic, payload=payload)
-                    L.l.info("Published HA discovery to {}, payload={}".format(subtopic, payload))
-                    index += 1
-            #else:
-            #    send_mqtt_ha(subtopic=subtopic, payload=payload)
-            P.discovery_timestamps[device_name] = datetime.datetime.now()
-
-        for item in change:
-            if item != 'updated_on' and item != 'source_host':
-                device_unique_id = device_name + '_' + item
-                subtopic = "{}/{}/state".format(device_type, device_unique_id)
-                value = "{}".format(getattr(obj, item))
-                # payload = P.sensor_template.replace("<device_class>", item).replace("<sensor_value>", value)
-                payload = value
-                send_mqtt_ha(subtopic=subtopic, payload=payload)
-
-
-        L.l.debug('Received ha obj={} change={} for rule parsing'.format(obj, change))
-        if hasattr(obj, Constant.JSON_PUBLISH_SOURCE_HOST):
-            source = getattr(obj, Constant.JSON_PUBLISH_SOURCE_HOST)
-        elif hasattr(obj, Constant.JSON_PUBLISH_SRC_HOST):
-            source = getattr(obj, Constant.JSON_PUBLISH_SRC_HOST)
-        else:
-            source = 'unknown'
-        # process all changes from all hosts
-        # if source == Constant.HOST_NAME or source is None:
-        try:
-            # extract only changed fields
-            if hasattr(obj, Constant.JSON_PUBLISH_FIELDS_CHANGED):
-                change = obj.last_commit_field_changed_list
-            else:
-                change = change
-            #generate standard messages for home_assistant
-            #print(change)
-        except Exception as ex:
-            L.l.exception('Error parsing ha rules, ex={}'.format(ex))
-
-def send_mqtt_ha(subtopic, payload):
-    transport.send_message_topic(topic=P.ha_topic + subtopic, json=payload)
+def send_mqtt_ha(topic, payload):
+    transport.send_message_topic(topic=topic, json=payload)
 
 def mqtt_on_message(client, userdata, msg):
     try:
@@ -130,6 +101,60 @@ def mqtt_on_message(client, userdata, msg):
         # L.l.info('Got ha mqtt {}={}'.format(msg.topic, payload))
     except Exception as ex:
         L.l.error('Error ha mqtt {} ex={}'.format(msg.payload, ex), exc_info=True)
+
+def announce_discovery(obj):
+    device_name, device_type, availability_topic = _get_variables(obj)
+    # publish a discoverable package periodically
+    if device_name in P.discovery_timestamps.keys():
+        last_discovery = P.discovery_timestamps[device_name]
+    else:
+        last_discovery = datetime.datetime.min
+
+    if (datetime.datetime.now() - last_discovery).total_seconds() > P.discovery_resent_period:
+        ha_field_list = obj.ha_fields.strip().split(',')
+        device_class_list = obj.ha_device_class.strip().split(',')
+        device_class_unit_list = obj.ha_device_class_unit.strip().split(',')
+        index = 0
+        for ha_field in ha_field_list:
+            device_class = device_class_list[index]
+            device_unit = device_class_unit_list[index]
+            sensor_unique_id = device_name + '_' + ha_field
+            sensor_name = device_name + ' ' + ha_field
+            subtopic_discovery = "{}/{}/{}/config".format(device_type, device_name, sensor_unique_id)
+            state_topic = TemplateSensorDiscovery.state_topic.replace(
+                "<ha_mqtt_prefix>", P.ha_topic).replace(
+                "<device_type>", device_type).replace(
+                "<unique_sensor_id>", sensor_unique_id)
+            payload = TemplateSensorDiscovery.__doc__.replace(
+                "<sensor_name>", sensor_name).replace(
+                "<unique_sensor_id>", sensor_unique_id).replace(
+                "<availability_topic>", availability_topic).replace(
+                "<state_topic>", state_topic).replace(
+                "<ha_mqtt_prefix>", P.ha_topic).replace(
+                "<device_type>", device_type).replace(
+                "<device_name>", device_name).replace(
+                "<unique_device_id>", device_name).replace(
+                "<device_class>", device_class).replace(
+                "<unit_of_measurement>", device_unit)
+            if ',"device_class":"",' in payload:
+                payload = payload.replace(',"device_class":"",', '').replace(
+                    '"unit_of_measurement":""', '')
+            send_mqtt_ha(topic=P.ha_topic + subtopic_discovery, payload=payload)
+            L.l.info("Published HA discovery to {}, payload={}".format(subtopic_discovery, payload))
+            index += 1
+        # else:
+        #    send_mqtt_ha(subtopic=subtopic, payload=payload)
+        P.discovery_timestamps[device_name] = datetime.datetime.now()
+
+def discovery():
+    cls_dict = dict([(name, cls) for name, cls in model.__dict__.items() if isinstance(cls, type)])
+    #sorted_keys = sorted(cls_dict)
+    for cls in cls_dict.values():
+        if issubclass(cls, HADiscoverableDevice) and cls.__name__ != "HADiscoverableDevice":
+            rec_list = cls.find()
+            if rec_list is not None:
+                for obj in rec_list:
+                    announce_discovery(obj)
 
 def thread_run():
     prctl.set_name("ha")
@@ -147,8 +172,9 @@ def init():
     P.ha_topic = get_json_param(Constant.P_MQTT_TOPIC_HA_SEND)
     P.mqtt_topic_receive = get_json_param(Constant.P_MQTT_TOPIC_HA_RECEIVE)
     P.mqtt_topic_receive_prefix = P.mqtt_topic_receive.replace('#', '')
+    discovery()
     mqtt_io.P.mqtt_client.message_callback_add(P.mqtt_topic_receive, mqtt_on_message)
     mqtt_io.add_message_callback(P.mqtt_topic_receive, mqtt_on_message)
-    thread_pool.add_interval_callable(thread_run, run_interval_second=1)
+    #thread_pool.add_interval_callable(thread_run, run_interval_second=30)
     dispatcher.connect(parse_rules, signal=Constant.SIGNAL_DB_CHANGE_FOR_RULES, sender=dispatcher.Any)
     P.initialised = True
